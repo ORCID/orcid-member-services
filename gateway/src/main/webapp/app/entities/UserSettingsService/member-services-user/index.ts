2c74790767805fc678b8003fcc02c98d06340389
Angel <a.montenegro@orcid.org>
2019-11-05 16:46:03 -0600

    Lets refactor it so we use a UserSettings and MemberSettings tables

--- gateway/src/main/webapp/app/entities/UserSettingsService/member-services-user/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './member-services-user.service';
-export * from './member-services-user-update.component';
-export * from './member-services-user-delete-dialog.component';
-export * from './member-services-user-detail.component';
-export * from './member-services-user.component';
-export * from './member-services-user.route';
