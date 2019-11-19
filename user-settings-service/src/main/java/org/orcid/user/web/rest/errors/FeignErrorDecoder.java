3e3e674bf3e78a1440e077adb0309cd821ab2f3b
Angel <a.montenegro@orcid.org>
2019-11-19 09:58:02 -0600

    Able to import users though CSV

--- user-settings-service/src/main/java/org/orcid/user/web/rest/errors/FeignErrorDecoder.java
+++ user-settings-service/src/main/java/org/orcid/user/web/rest/errors/FeignErrorDecoder.java
@@ -1,5 +1,10 @@
 package org.orcid.user.web.rest.errors;
 
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -16,16 +21,31 @@ public class FeignErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-
-        switch (response.status()) {
-        case 400:
-            logger.error("Status code " + response.status() + ", methodKey = " + methodKey);
-        case 404: {
-            logger.error("Error took place when using Feign client to send HTTP Request. Status code " + response.status() + ", methodKey = " + methodKey);
-            return new ResponseStatusException(HttpStatus.valueOf(response.status()), "Not found");
+        try {            
+            switch (response.status()) {
+            case 400:
+                logger.error("Feign client. Status code " + response.status() + ", methodKey = " + methodKey);
+                return new ResponseStatusException(HttpStatus.valueOf(response.status()), getErrorMessage(response));
+            case 404: {
+                logger.error("Feign client. Status code " + response.status() + ", methodKey = " + methodKey);
+                return new ResponseStatusException(HttpStatus.valueOf(response.status()), getErrorMessage(response));
+            }
+            default:
+                return new Exception(response.reason());
+            }
+        } catch (Exception e) {
+            logger.error("Unable to parse Feign exception");
+            throw new RuntimeException(e);
         }
-        default:
-            return new Exception(response.reason());
+    }
+
+    private String getErrorMessage(Response response) throws JSONException {        
+        try {
+            JSONObject j = new JSONObject(IOUtils.toString(response.body().asInputStream(), java.nio.charset.StandardCharsets.UTF_8));
+            return j.getString("title");
+        } catch (IOException e) {
+            logger.error("read conflict response body exception. {}", e.toString());
+            return "Unknown: " + e.toString();
         }
     }
 
