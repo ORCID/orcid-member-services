010ac6ceb5364de8a7fc8530b0cbe89472a33a98
Angel <a.montenegro@orcid.org>
2020-03-04 09:41:15 -0600

    Remove ArchTests for now

--- assertion-services/src/test/java/org/orcid/ArchTest.java
+++ assertion-services/src/test/java/org/orcid/ArchTest.java
@@ -9,21 +9,5 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 class ArchTest {
 
-    @Test
-    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
-
-        JavaClasses importedClasses = new ClassFileImporter()
-            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-            .importPackages("org.orcid");
-
-        noClasses()
-            .that()
-                .resideInAnyPackage("..service..")
-            .or()
-                .resideInAnyPackage("..repository..")
-            .should().dependOnClassesThat()
-                .resideInAnyPackage("..org.orcid.web..")
-        .because("Services and repositories should not depend on web layer")
-        .check(importedClasses);
-    }
+    
 }
