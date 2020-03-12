5a84231ae698daba511354fa45bb29384845b372
Angel <a.montenegro@orcid.org>
2020-03-12 10:17:42 -0600

    Ready to go

--- user-settings-service/src/main/java/org/orcid/user/service/dto/UserDTO.java
+++ user-settings-service/src/main/java/org/orcid/user/service/dto/UserDTO.java
@@ -19,20 +19,21 @@ public class UserDTO implements Serializable {
     private String firstName;
     private String firstNameError;
     private String lastName;
-    private String lastNameError;        
+    private String lastNameError;
     private Boolean mainContact;
     private Boolean assertionServicesEnabled;
 
     // MemberSettings data
     private String salesforceId;
     private String salesforceIdError;
-    
+
     // Metadata
     private String jhiUserId;
     private String createdBy;
     private Instant createdDate;
     private String lastModifiedBy;
-    private Instant lastModifiedDate;    
+    private Instant lastModifiedDate;
+    private Boolean deleted;
 
     public String getId() {
         return id;
@@ -170,6 +171,14 @@ public class UserDTO implements Serializable {
         this.jhiUserId = jhiUserId;
     }
 
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -177,6 +186,7 @@ public class UserDTO implements Serializable {
         result = prime * result + ((assertionServicesEnabled == null) ? 0 : assertionServicesEnabled.hashCode());
         result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
         result = prime * result + ((createdDate == null) ? 0 : createdDate.hashCode());
+        result = prime * result + ((deleted == null) ? 0 : deleted.hashCode());
         result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
         result = prime * result + ((firstNameError == null) ? 0 : firstNameError.hashCode());
         result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -217,6 +227,11 @@ public class UserDTO implements Serializable {
             if (other.createdDate != null)
                 return false;
         } else if (!createdDate.equals(other.createdDate))
+            return false;
+        if (deleted == null) {
+            if (other.deleted != null)
+                return false;
+        } else if (!deleted.equals(other.deleted))
             return false;
         if (firstName == null) {
             if (other.firstName != null)
@@ -300,7 +315,8 @@ public class UserDTO implements Serializable {
         result.setCreatedBy(us.getCreatedBy());
         result.setCreatedDate(us.getCreatedDate());
         result.setLastModifiedBy(us.getLastModifiedBy());
-        result.setLastModifiedDate(us.getLastModifiedDate());        
+        result.setLastModifiedDate(us.getLastModifiedDate());
+        result.setDeleted(us.getDeleted());
         return result;
     }
 
@@ -309,6 +325,7 @@ public class UserDTO implements Serializable {
         return "UserDTO [id=" + id + ", login=" + login + ", loginError=" + loginError + ", password=" + password + ", firstName=" + firstName + ", firstNameError="
                 + firstNameError + ", lastName=" + lastName + ", lastNameError=" + lastNameError + ", mainContact=" + mainContact + ", assertionServicesEnabled="
                 + assertionServicesEnabled + ", salesforceId=" + salesforceId + ", salesforceIdError=" + salesforceIdError + ", jhiUserId=" + jhiUserId + ", createdBy="
-                + createdBy + ", createdDate=" + createdDate + ", lastModifiedBy=" + lastModifiedBy + ", lastModifiedDate=" + lastModifiedDate + "]";
-    }    
+                + createdBy + ", createdDate=" + createdDate + ", lastModifiedBy=" + lastModifiedBy + ", lastModifiedDate=" + lastModifiedDate + ", deleted=" + deleted
+                + "]";
+    }
 }
