fbb0c546b1279b6aacac47b0991aa4c82c43c677
Angel <a.montenegro@orcid.org>
2020-03-20 15:40:22 -0600

    Use a tag for the releases

--- release.sh
+++ release.sh
@@ -6,21 +6,22 @@ git tag $1
 git push origin --tags --no-verify
 echo "jhipster-registry"
 cd jhipster-registry
-bash mvnw -ntp -Pdev verify jib:dockerBuild
+bash mvnw -ntp -Pdev clean verify jib:dockerBuild -Drelease.tag=$1
 echo "gateway"
 cd ../gateway
-bash mvnw -ntp -Pdev verify jib:dockerBuild
+bash mvnw -ntp -Pdev clean verify jib:dockerBuild -Drelease.tag=$1
 echo "oauth2-service"
 cd ../oauth2-service
-bash mvnw -ntp -Pdev verify jib:dockerBuild
+bash mvnw -ntp -Pdev clean verify jib:dockerBuild -Drelease.tag=$1
 echo "user-settings-service"
 cd ../user-settings-service
-bash mvnw -ntp -Pdev verify jib:dockerBuild
+bash mvnw -ntp -Pdev clean verify jib:dockerBuild -Drelease.tag=$1
 echo "assertion-services"
 cd ../assertion-services
-bash mvnw -ntp -Pdev verify jib:dockerBuild
+bash mvnw -ntp -Pdev clean verify jib:dockerBuild -Drelease.tag=$1
 echo "Running docker compose"
 cd ../
 docker-compose down
+export TAG=$1
 docker-compose up -d
 echo "Done"
