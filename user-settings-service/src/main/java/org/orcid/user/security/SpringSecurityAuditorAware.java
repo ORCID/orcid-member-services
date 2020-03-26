74451335a3007cd5223b943ca04bd6098c50ad2d
George Nash <bobcaprice@hotmail.com>
2020-03-26 00:19:19 -0500

    Refactoring and adding unit test

--- user-settings-service/src/main/java/org/orcid/user/security/SpringSecurityAuditorAware.java
+++ user-settings-service/src/main/java/org/orcid/user/security/SpringSecurityAuditorAware.java
@@ -4,6 +4,7 @@ import org.orcid.user.config.Constants;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +13,12 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
+	
+	@Autowired
+	private SecurityUtils securityUtils;
 
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+        return Optional.of(securityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
     }
 }
