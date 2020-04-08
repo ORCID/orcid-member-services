4875eeac2b0d8606d78a1d84ffd7e09eca11896b
George Nash <bobcaprice@hotmail.com>
2020-04-08 12:05:13 +0100

    Reorganised and refactored, fixed caching bug so users can be recreated

--- oauth2-service/src/main/java/org/orcid/auth/repository/UserRepository.java
+++ oauth2-service/src/main/java/org/orcid/auth/repository/UserRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import org.orcid.auth.service.cache.UserCaches;
+
 import java.util.List;
 import java.util.Optional;
 import java.time.Instant;
@@ -19,20 +21,16 @@ import java.time.Instant;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
-    String USERS_BY_LOGIN_CACHE = "usersByLogin";
-
-    String USERS_BY_EMAIL_CACHE = "usersByEmail";    
-    
     Optional<User> findOneByActivationKey(String activationKey);
 
     List<User> findAllByActivatedIsFalseAndActivationKeyIsNotNullAndCreatedDateBefore(Instant dateTime);
 
     Optional<User> findOneByResetKey(String resetKey);
 
-    @Cacheable(cacheNames = USERS_BY_EMAIL_CACHE)
+    @Cacheable(cacheNames = UserCaches.USERS_BY_EMAIL_CACHE)
     Optional<User> findOneByEmailIgnoreCase(String email);
 
-    @Cacheable(cacheNames = USERS_BY_LOGIN_CACHE)
+    @Cacheable(cacheNames = UserCaches.USERS_BY_LOGIN_CACHE)
     Optional<User> findOneByLogin(String login);
     
     Optional<User> findOneById(String id);
