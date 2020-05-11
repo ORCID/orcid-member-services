75c912da9b11a7033d4549db56692f7512287f75
George Nash <bobcaprice@hotmail.com>
2020-05-11 17:41:39 +0100

    Renaming following removal of UserSettings

--- gateway/src/main/webapp/app/entities/UserSettingsService/member-settings/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './member-settings.service';
-export * from './member-settings-import-dialog.component';
-export * from './member-settings-update.component';
-export * from './member-settings-delete-dialog.component';
-export * from './member-settings-detail.component';
-export * from './member-settings.component';
-export * from './member-settings.route';
