f6899fce1046157dc104dbbedeb8a29dd6a13028
George Nash <bobcaprice@hotmail.com>
2020-05-11 17:41:39 +0100

    Renaming following removal of UserSettings

---
+++ gateway/src/test/javascript/spec/app/entities/MSUserService/ms-members/ms-member.service.spec.ts
@@ -0,0 +1,174 @@
+/* tslint:disable max-line-length */
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import * as moment from 'moment';
+import { DATE_TIME_FORMAT } from 'app/shared/constants/input.constants';
+import { MSMemberService } from 'app/entities/MSUserService/ms-members/ms-member.service';
+import { IMSMember, MSMember } from 'app/shared/model/MSUserService/ms-member.model';
+
+describe('Service Tests', () => {
+  describe('MSMember Service', () => {
+    let injector: TestBed;
+    let service: MSMemberService;
+    let httpMock: HttpTestingController;
+    let elemDefault: IMSMember;
+    let expectedResult;
+    let currentDate: moment.Moment;
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule]
+      });
+      expectedResult = {};
+      injector = getTestBed();
+      service = injector.get(MSMemberService);
+      httpMock = injector.get(HttpTestingController);
+      currentDate = moment();
+
+      elemDefault = new MSMember(
+        'ID',
+        'AAAAAAA',
+        'AAAAAAA',
+        'AAAAAAA',
+        'AAAAAAA',
+        'AAAAAAA',
+        false,
+        false,
+        'AAAAAAA',
+        currentDate,
+        'AAAAAAA',
+        currentDate
+      );
+    });
+
+    describe('Service methods', () => {
+      it('should find an element', async () => {
+        const returnedFromService = Object.assign(
+          {
+            createdDate: currentDate.format(DATE_TIME_FORMAT),
+            lastModifiedDate: currentDate.format(DATE_TIME_FORMAT)
+          },
+          elemDefault
+        );
+        service
+          .find('123')
+          .pipe(take(1))
+          .subscribe(resp => (expectedResult = resp));
+
+        const req = httpMock.expectOne({ method: 'GET' });
+        req.flush(returnedFromService);
+        expect(expectedResult).toMatchObject({ body: elemDefault });
+      });
+
+      it('should create a MSMember', async () => {
+        const returnedFromService = Object.assign(
+          {
+            id: 'ID',
+            createdDate: currentDate.format(DATE_TIME_FORMAT),
+            lastModifiedDate: currentDate.format(DATE_TIME_FORMAT)
+          },
+          elemDefault
+        );
+        const expected = Object.assign(
+          {
+            createdDate: currentDate,
+            lastModifiedDate: currentDate
+          },
+          returnedFromService
+        );
+        service
+          .create(new MSMember(null))
+          .pipe(take(1))
+          .subscribe(resp => (expectedResult = resp));
+        const req = httpMock.expectOne({ method: 'POST' });
+        req.flush(returnedFromService);
+        expect(expectedResult).toMatchObject({ body: expected });
+      });
+
+      it('should update a MSMember', async () => {
+        const returnedFromService = Object.assign(
+          {
+            clientId: 'BBBBBB',
+            clientName: 'BBBBBB',
+            clientSecret: 'BBBBBB',
+            salesforceId: 'BBBBBB',
+            parentSalesforceId: 'BBBBBB',
+            isConsortiumLead: true,
+            assertionServiceEnabled: true,
+            createdBy: 'BBBBBB',
+            createdDate: currentDate.format(DATE_TIME_FORMAT),
+            lastModifiedBy: 'BBBBBB',
+            lastModifiedDate: currentDate.format(DATE_TIME_FORMAT)
+          },
+          elemDefault
+        );
+
+        const expected = Object.assign(
+          {
+            createdDate: currentDate,
+            lastModifiedDate: currentDate
+          },
+          returnedFromService
+        );
+        service
+          .update(expected)
+          .pipe(take(1))
+          .subscribe(resp => (expectedResult = resp));
+        const req = httpMock.expectOne({ method: 'PUT' });
+        req.flush(returnedFromService);
+        expect(expectedResult).toMatchObject({ body: expected });
+      });
+
+      it('should return a list of MSMember', async () => {
+        const returnedFromService = Object.assign(
+          {
+            clientId: 'BBBBBB',
+            clientName: 'BBBBBB',
+            clientSecret: 'BBBBBB',
+            salesforceId: 'BBBBBB',
+            parentSalesforceId: 'BBBBBB',
+            isConsortiumLead: true,
+            assertionServiceEnabled: true,
+            createdBy: 'BBBBBB',
+            createdDate: currentDate.format(DATE_TIME_FORMAT),
+            lastModifiedBy: 'BBBBBB',
+            lastModifiedDate: currentDate.format(DATE_TIME_FORMAT)
+          },
+          elemDefault
+        );
+        const expected = Object.assign(
+          {
+            createdDate: currentDate,
+            lastModifiedDate: currentDate
+          },
+          returnedFromService
+        );
+        service
+          .query(expected)
+          .pipe(
+            take(1),
+            map(resp => resp.body)
+          )
+          .subscribe(body => (expectedResult = body));
+        const req = httpMock.expectOne({ method: 'GET' });
+        req.flush([returnedFromService]);
+        httpMock.verify();
+        expect(expectedResult).toContainEqual(expected);
+      });
+
+      it('should delete a MSMember', async () => {
+        const rxPromise = service.delete('123').subscribe(resp => (expectedResult = resp.ok));
+
+        const req = httpMock.expectOne({ method: 'DELETE' });
+        req.flush({ status: 200 });
+        expect(expectedResult);
+      });
+    });
+
+    afterEach(() => {
+      httpMock.verify();
+    });
+  });
+});
