7a99c5436d3fe6b1250e739a2682a343633ec874
George Nash <bobcaprice@hotmail.com>
2020-05-12 11:42:17 +0100

    Further refactoring, renaming of oauth2service, packages, addresses etc

---
+++ user-service/src/test/java/org/orcid/user/upload/impl/MembersCsvReaderTest.java
@@ -0,0 +1,48 @@
+package org.orcid.user.upload.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.jupiter.api.Test;
+import org.orcid.user.domain.Member;
+import org.orcid.user.upload.MembersUpload;
+import org.orcid.user.upload.impl.MembersCsvReader;
+
+class MembersCsvReaderTest {
+
+	private MembersCsvReader reader = new MembersCsvReader();
+
+	@Test
+	void testReadMembersUpload() throws IOException {
+		InputStream inputStream = getClass().getResourceAsStream("/members.csv");
+		MembersUpload upload = reader.readMembersUpload(inputStream);
+
+		assertEquals(2, upload.getMembers().size());
+
+		Member one = upload.getMembers().get(0);
+		Member two = upload.getMembers().get(1);
+
+		assertTrue(one.getAssertionServiceEnabled());
+		assertFalse(two.getAssertionServiceEnabled());
+
+		assertEquals("client-id-1", one.getClientId());
+		assertEquals("client-id-2", two.getClientId());
+
+		assertFalse(one.getIsConsortiumLead());
+		assertFalse(two.getIsConsortiumLead());
+
+		assertEquals("salesforce-1", one.getSalesforceId());
+		assertEquals("salesforce-2", two.getSalesforceId());
+
+		assertEquals("salesforce-parent", one.getParentSalesforceId());
+		assertEquals("salesforce-parent", two.getParentSalesforceId());
+
+		assertEquals("some-client-name", one.getClientName());
+		assertEquals("some-other-client-name", two.getClientName());
+	}
+
+}
