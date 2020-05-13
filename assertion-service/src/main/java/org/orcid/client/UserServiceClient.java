ef58dc3ab343173096472c72d3c7db612503fbcd
George Nash <bobcaprice@hotmail.com>
2020-05-13 12:54:37 +0100

    Refactoring

--- assertion-service/src/main/java/org/orcid/client/UserServiceClient.java
+++ assertion-service/src/main/java/org/orcid/client/UserServiceClient.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import javax.ws.rs.core.MediaType;
 
-import org.orcid.domain.AssertionsUser;
+import org.orcid.domain.AssertionServiceUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +17,7 @@ public interface UserServiceClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/users/{loginOrId}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
     @HystrixProperty(name = "hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds", value = "5000")
-    ResponseEntity<AssertionsUser> getUser(@PathVariable("loginOrId") String loginOrId);
+    ResponseEntity<AssertionServiceUser> getUser(@PathVariable("loginOrId") String loginOrId);
     
     @RequestMapping(method = RequestMethod.POST, value = "/api/users", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
     @HystrixProperty(name = "hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds", value = "5000")
