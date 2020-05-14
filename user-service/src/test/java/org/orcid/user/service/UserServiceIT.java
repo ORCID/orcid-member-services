aa65494cf42f3a53fee54961a5c48c34851fa26a
George Nash <bobcaprice@hotmail.com>
2020-05-14 12:59:50 +0100

    Removal of unused IT

--- user-service/src/test/java/org/orcid/user/service/UserServiceIT.java
+++ user-service/src/test/java/org/orcid/user/service/UserServiceIT.java
@@ -159,17 +159,4 @@ public class UserServiceIT {
         assertThat(maybeDbUser).contains(dbUser);
     }
 
-    @Test
-    public void assertThatAnonymousUserIsNotGet() {
-        user.setLogin(Constants.ANONYMOUS_USER);
-        if (!userRepository.findOneByLogin(Constants.ANONYMOUS_USER).isPresent()) {
-            userRepository.save(user);
-        }
-        final PageRequest pageable = PageRequest.of(0, (int) userRepository.count());
-        final Page<UserDTO> allManagedUsers = userService.getAllManagedUsers(pageable);
-        assertThat(allManagedUsers.getContent().stream()
-            .noneMatch(user -> Constants.ANONYMOUS_USER.equals(user.getLogin())))
-            .isTrue();
-    }
-
 }
