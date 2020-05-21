39bb23746e8e1bdae7f84496edfd8a818e1698a1
George Nash <bobcaprice@hotmail.com>
2020-05-21 18:05:31 +0100

    Removed assertions enabled field from user, now set according to member

--- user-service/src/main/java/org/orcid/user/service/MemberService.java
+++ user-service/src/main/java/org/orcid/user/service/MemberService.java
@@ -1,6 +1,7 @@
 package org.orcid.user.service;
 
 import org.orcid.user.client.MemberServiceClient;
+import org.orcid.user.service.member.MemberServiceMember;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,21 @@ public class MemberService {
 	private MemberServiceClient memberServiceClient;
 	
 	public boolean memberExistsWithSalesforceId(String salesforceId) {
-		ResponseEntity<?> response = memberServiceClient.getMember(salesforceId);
+		ResponseEntity<MemberServiceMember> response = memberServiceClient.getMember(salesforceId);
 		if (response.getStatusCode().is2xxSuccessful()) {
 			return true;
+		}
+		if (response.getStatusCodeValue() == 404) {
+			return false;
+		}
+		
+		throw new RuntimeException("Error contacting member service");
+	}
+	
+	public boolean memberExistsWithSalesforceIdAndAssertionsEnabled(String salesforceId) {
+		ResponseEntity<MemberServiceMember> response = memberServiceClient.getMember(salesforceId);
+		if (response.getStatusCode().is2xxSuccessful()) {
+			return response.getBody().getAssertionServiceEnabled();
 		}
 		if (response.getStatusCodeValue() == 404) {
 			return false;
