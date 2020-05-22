193dc7b169b9a65f3182b142027d0747b6c3d143
lizkrznarich <e.krznarich@orcid.org>
2020-05-22 17:33:41 -0500

    add logic for retrieving member info and update landing page text

--- gateway/src/main/webapp/app/landing-page/landing-page.component.ts
+++ gateway/src/main/webapp/app/landing-page/landing-page.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 import { ActivatedRoute } from '@angular/router';
 import { KEYUTIL, KJUR } from 'jsrsasign';
 import { LandingPageService } from './landing-page.service';
-import { AccountService, Account } from 'app/core';
+import { IMSUser } from 'app/shared/model/MSUserService/ms-user.model';
+import { MSUserService } from 'app/entities/MSUserService/ms-users/ms-user.service';
+import { IMSMember } from 'app/shared/model/MSUserService/ms-member.model';
+import { MSMemberService } from 'app/entities/MSUserService/ms-members/ms-member.service';
 
 @Component({
   selector: 'jhi-landing-page',
@@ -14,11 +18,11 @@ export class LandingPageComponent implements OnInit {
 
   const sandboxIssuer: String = 'https://sandbox.orcid.org';
   const sandboxOauthUrl: String = 'https://sandbox.orcid.org/oauth/authorize';
-  const clientId: String = 'APP-99MR3GU01PBQK8FP';
   // TODO: should be configurable
   const redirectUri: String = 'http://localhost:8080/landing-page';
   const sandboxKey = {'kty': 'RSA', 'e': 'AQAB', 'use': 'sig', 'kid': 'sandbox-orcid-org-3hpgosl3b6lapenh1ewsgdob3fawepoj', 'n': 'pl-jp-kTAGf6BZUrWIYUJTvqqMVd4iAnoLS6vve-KNV0q8TxKvMre7oi9IulDcqTuJ1alHrZAIVlgrgFn88MKirZuTqHG6LCtEsr7qGD9XyVcz64oXrb9vx4FO9tLNQxvdnIWCIwyPAYWtPMHMSSD5oEVUtVL_5IaxfCJvU-FchdHiwfxvXMWmA-i3mcEEe9zggag2vUPPIqUwbPVUFNj2hE7UsZbasuIToEMFRZqSB6juc9zv6PEUueQ5hAJCEylTkzMwyBMibrt04TmtZk2w9DfKJR91555s2ZMstX4G_su1_FqQ6p9vgcuLQ6tCtrW77tta-Rw7McF_tyPmvnhQ'};
 
+  showConnectionExists: Boolean = false;
   showOauth: Boolean = false;
   showDenied: Boolean = false;
   showError: Boolean = true;
@@ -26,42 +30,70 @@ export class LandingPageComponent implements OnInit {
   issuer: String;
   key: any;
   oauthUrl: String;
-  
-  constructor(private eventManager: JhiEventManager, private landingPageService: LandingPageService, private route: ActivatedRoute) {
+  clientName: String;
+  clientId: String;
+  orcidId: String;
+
+  constructor(
+    private eventManager: JhiEventManager,
+    private landingPageService: LandingPageService,
+    protected msUserService: MSUserService,
+    protected msMemberService: MSMemberService,
+    private route: ActivatedRoute
+  ) {
     // TODO: need to make this switch from sandbox to prod
     this.issuer = this.sandboxIssuer;
     this.key = this.sandboxKey;
-    this.oauthUrl = this.sandboxOauthUrl + '?response_type=token&redirect_uri=' + this.redirectUri + '&client_id=' + this.clientId + '&scope=/activities/update openid';
   }
 
   ngOnInit() {
-    let id_token = this.getFragmentParameterByName('id_token');
-    let state_param = this.getFragmentParameterByName('state');
-    
-    if (id_token != null && id_token != '') {
-      if (this.checkSig(id_token)) {
-        this.submitIdTokenData(id_token, state_param);
-      } else {
-        this.showErrorElement();
-      }
-    } else {
-      this.showOauthButton();
-      state_param = this.getQueryParameterByName('state');
-      let error = this.getFragmentParameterByName('error');
-      if (error != null && error != '') {
-        if (error === 'access_denied') {
-          this.submitUserDenied(state_param);
-          this.showErrorElement();
-        } else {
-          this.showErrorElement();
+    let id_token_fragment = this.getFragmentParameterByName('id_token');
+    let state_param = this.getQueryParameterByName('state');
+
+    this.landingPageService.getOrcidConnectionRecord(state_param)
+      .subscribe(
+        (res: HttpResponse<any>) => {
+          console.log(res.body);
+          //Check if id token already exists in DB (user previously granted permission)
+          if(res.body.idToken != null && res.body.idToken != ''){
+            this.orcidId = res.body.orcid;
+            this.showConnectionExistsElement();
+          } else {
+            //Check if id token exists in URL (user just granted permission)
+            if (id_token_fragment != null && id_token_fragment != '') {
+              if (this.checkSig(id_token_fragment)) {
+                this.submitIdTokenData(id_token_fragment, state_param);
+              } else {
+                this.showErrorElement();
+              }
+            } else {
+              //Build Oauth URL
+              this.buildOauthUrl(res.body.ownerId);
+              this.showOauthButton();
+              state_param = this.getQueryParameterByName('state');
+              let error = this.getFragmentParameterByName('error');
+              //Check if user denied permission
+              if (error != null && error != '') {
+                if (error === 'access_denied') {
+                  this.submitUserDenied(state_param);
+                  this.showErrorElement();
+                } else {
+                  this.showErrorElement();
+                }
+              } else {
+                if (state_param != null) {
+                  this.oauthUrl = this.oauthUrl + '&state=' + state_param;
+                }
+                this.showOauthButton();
+              }
+            }
+
+          }
+        },
+        (res: HttpErrorResponse) => {
+          console.log('error');
         }
-      } else {
-        if (state_param != null) {
-          this.oauthUrl = this.oauthUrl + '&state=' + state_param;
-        }
-        this.showOauthButton();
-      }
-    }
+    );
   }
 
   getFragmentParameterByName(name: String): String {
@@ -75,11 +107,15 @@ export class LandingPageComponent implements OnInit {
       name = name.replace(/[\[\]]/g, '\\$&');
       let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
           results = regex.exec(window.location.href);
-      if (!results) return null;
-      if (!results[2]) return '';
+      if (!results) {
+        return null;
+      }
+      if (!results[2]) {
+        return '';
+      }
       return decodeURIComponent(results[2].replace(/\+/g, ' '));
   }
-  
+
   checkSig(id_token: String) {
     let pubKey = KEYUTIL.getKey(this.key);
     return KJUR.jws.JWS.verifyJWT(id_token, pubKey, {
@@ -106,6 +142,36 @@ export class LandingPageComponent implements OnInit {
         this.showErrorElement();
       }
     );
+  }
+
+  buildOauthUrl(ownerId: string) {
+    this.msUserService.find(ownerId).subscribe(
+      (res: HttpResponse<IMSUser>) => {
+        if( res.body.salesforceId != null && res.body.salesforceId != '' ){
+          this.msMemberService.find(res.body.salesforceId).subscribe(
+            (res: HttpResponse<IMSMember>) => {
+              this.clientName = res.body.clientName;
+              this.clientId = res.body.clientId;
+              this.oauthUrl = this.sandboxOauthUrl + '?response_type=token&redirect_uri=' + this.redirectUri + '&client_id=' + this.clientId + '&scope=/activities/update openid';
+            },
+            () => {
+              console.log("error")
+            }
+          );
+        }
+      },
+      () => {
+        console.log("error")
+      });
+
+  }
+
+  showConnectionExistsElement(): void {
+    this.showOauth = false;
+    this.showDenied = false;
+    this.showError = false;
+    this.showSuccess = false;
+    this.showConnectionExists = true;
   }
 
   showErrorElement(): void {
