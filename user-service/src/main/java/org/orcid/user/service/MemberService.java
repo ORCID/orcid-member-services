de198ed9fcf07e60f614f9ef6fad6efb7a4bf77a
Daniel Palafox <dp852@hotmail.com>
2020-06-05 14:55:50 -0500

    Displaying user friendly error message at import members

--- user-service/src/main/java/org/orcid/user/service/MemberService.java
+++ user-service/src/main/java/org/orcid/user/service/MemberService.java
@@ -1,8 +1,10 @@
 package org.orcid.user.service;
 
+import feign.FeignException;
 import org.orcid.user.client.MemberServiceClient;
 import org.orcid.user.service.member.MemberServiceMember;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,34 @@ public class MemberService {
 	private MemberServiceClient memberServiceClient;
 	
 	public boolean memberExistsWithSalesforceId(String salesforceId) {
-		ResponseEntity<MemberServiceMember> response = memberServiceClient.getMember(salesforceId);
-		if (response.getStatusCode().is2xxSuccessful()) {
-			return true;
-		}
-		if (response.getStatusCodeValue() == 404) {
-			return false;
-		}
-		
+	    try {
+            ResponseEntity<MemberServiceMember> response = memberServiceClient.getMember(salesforceId);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return true;
+            }
+
+            if (response.getStatusCode().is4xxClientError()) {
+                return false;
+            }
+
+        } catch (FeignException ex) {
+
+            HttpStatus status = HttpStatus.resolve(ex.status());
+            if (status == null) {
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+
+            System.out.println(status.value());
+            if (status.is2xxSuccessful()) {
+                return true;
+            }
+            if (status.is4xxClientError()) {
+                return false;
+            }
+
+        }
+
 		throw new RuntimeException("Error contacting member service");
 	}
 	
