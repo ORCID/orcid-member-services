90787f462ad2fc62cdfb38ff62d1bcaf4fc0b0a6
Daniel Palafox <dp852@hotmail.com>
2020-07-02 17:49:18 -0500

    fix import error messages

--- member-service/src/main/java/org/orcid/member/upload/impl/MemberCsvReader.java
+++ member-service/src/main/java/org/orcid/member/upload/impl/MemberCsvReader.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.time.Instant;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.orcid.member.domain.Member;
@@ -47,7 +48,7 @@ public class MemberCsvReader implements MembersUploadReader {
                     long index = line.getRecordNumber();
                     Member member = parseLine(line, now);
 
-                    if (!MemberValidator.validate(member)) {
+                    if (member.getError() != null || !MemberValidator.validate(member)) {
                         upload.addError(index, member.getError());
                     } else {
                         upload.addMember(member);
@@ -75,23 +76,45 @@ public class MemberCsvReader implements MembersUploadReader {
 		} else {
 			member.setAssertionServiceEnabled(false);
 		}
-		member.setClientId(record.get("clientId"));
+
+        if (validateField(record, "clientId", "Client id should not be empty", member)) {
+            member.setClientId(record.get("clientId"));
+        }
+
 		Boolean isConsortiumLead = false;
 		if (record.isSet("isConsortiumLead")) {
 			isConsortiumLead = Boolean.parseBoolean(record.get("isConsortiumLead"));
 		}
 		member.setIsConsortiumLead(isConsortiumLead);
-		member.setSalesforceId(record.get("salesforceId"));
+
+		if (validateField(record, "salesforceId", "Salesforce id should not be empty", member)) {
+            member.setSalesforceId(record.get("salesforceId"));
+        }
 
 		if (!isConsortiumLead) {
-			member.setParentSalesforceId(record.get("parentSalesforceId"));
+            if (validateField(record,"parentSalesforceId", "Parent salesforce id should not be empty if it is not a consortium lead", member)) {
+                member.setParentSalesforceId(record.get("parentSalesforceId"));
+            }
 		}
 		if (record.isSet("clientName")) {
-			member.setClientName(record.get("clientName"));
+            if (validateField(record, "clientName", "Member name should not be empty", member)) {
+                member.setClientName(record.get("clientName"));
+            }
 		}
+
 		member.setCreatedDate(now);
 		member.setLastModifiedDate(now);
 		return member;
 	}
+
+    private boolean validateField(CSVRecord record, String value, String error, Member member) {
+        try {
+            record.get(value);
+        } catch (IllegalArgumentException e) {
+            member.setError(error);
+            return false;
+        }
+        return true;
+    }
 
 }
