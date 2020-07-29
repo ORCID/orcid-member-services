50255da71858f555a418c5da6fab4c288aa063e2
Daniel Palafox <dp852@hotmail.com>
2020-07-29 17:40:22 -0500

    fix admin id in affiliations

--- gateway/src/main/webapp/app/entities/AssertionService/assertion/assertion-detail.component.ts
+++ gateway/src/main/webapp/app/entities/AssertionService/assertion/assertion-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { IAssertion } from 'app/shared/model/AssertionService/assertion.model';
 import { ORCID_BASE_URL } from 'app/app.constants';
+import { UserService } from 'app/core';
 
 @Component({
   selector: 'jhi-assertion-detail',
@@ -11,12 +12,16 @@ import { ORCID_BASE_URL } from 'app/app.constants';
 export class AssertionDetailComponent implements OnInit {
   assertion: IAssertion;
   orcidBaseUrl: string = ORCID_BASE_URL;
+  ownerId: string;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, protected userService: UserService) {}
 
   ngOnInit() {
     this.activatedRoute.data.subscribe(({ assertion }) => {
       this.assertion = assertion;
+      this.userService.find(this.assertion.ownerId).subscribe(user => {
+        this.ownerId = user.body.login;
+      });
     });
   }
 
