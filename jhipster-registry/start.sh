9daab1f417fbc426d67c41fe232920ecf1e10d46
Will Simpson <w.simpson@orcid.org>
2020-08-26 21:44:51 +0100

    Automatically download jhipster registry jar if possible.

--- jhipster-registry/start.sh
+++ jhipster-registry/start.sh
@@ -1,6 +1,12 @@
-if [ ! -f jhipster-registry-6.1.2.jar ]; then
-    echo "Download jhipster-registry-6.1.2.jar - https://github.com/jhipster/jhipster-registry/releases - and place in this directory"
+JHIPSTER_URL=https://github.com/jhipster/jhipster-registry/releases/download/v6.1.2/jhipster-registry-6.1.2.jar
+JHIPSTER_JAR=`basename $JHIPSTER_URL`
+
+if [ ! -f $JHIPSTER_JAR ]; then
+    if ! curl -L $JHIPSTER_URL --output $JHIPSTER_JAR
+    then
+        echo "Download $JHIPSTER_JAR - https://github.com/jhipster/jhipster-registry/releases - and place in this directory"
+    fi
 fi
 
-java -jar jhipster-registry-6.1.2.jar --spring.security.user.password=admin --jhipster.security.authentication.jwt.secret=my-secret-key-which-should-be-changed-in-production-and-be-base64-encoded --spring.cloud.config.server.composite.0.type=native --spring.cloud.config.server.composite.0.search-locations=file:./central-config
+java -jar $JHIPSTER_JAR --spring.security.user.password=admin --jhipster.security.authentication.jwt.secret=my-secret-key-which-should-be-changed-in-production-and-be-base64-encoded --spring.cloud.config.server.composite.0.type=native --spring.cloud.config.server.composite.0.search-locations=file:./central-config
 
