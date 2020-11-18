12fd4db6c9751961f804574ebbbabf1c7d0e2c63
Camelia-Orcid <c.dumitru@orcid.org>
2020-11-18 16:58:07 +0000

    When deleting members allow to skip the mainContact check for it´s users

--- member-service/src/main/java/org/orcid/member/client/UserServiceClient.java
+++ member-service/src/main/java/org/orcid/member/client/UserServiceClient.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
@@ -38,6 +39,6 @@ public interface UserServiceClient {
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/api/users/{loginOrId}", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
     @HystrixProperty(name = "hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds", value = "5000")
-    ResponseEntity<Void> deleteUser(@PathVariable("loginOrId") String loginOrId);
+    ResponseEntity<Void> deleteUser(@PathVariable("loginOrId") String loginOrId, @RequestParam(value = "noMainContactCheck", required = false) boolean noMainContactCheck);
 
 }
