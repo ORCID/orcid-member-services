1eeace162b1c10067e2f9c2a580f7b330362b9b8
Camelia <62257307+Camelia-Orcid@users.noreply.github.com>
2021-01-14 09:02:50 +0000

    Added the salesforce id, client id when init DB from application property files.
    At this point we only have dev and main config.

--- member-service/src/main/java/org/orcid/member/config/DatabaseConfiguration.java
+++ member-service/src/main/java/org/orcid/member/config/DatabaseConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
@@ -52,9 +53,10 @@ public class DatabaseConfiguration {
     }
 
     @Bean
-    public Mongobee mongobee(MongoClient mongoClient, MongoTemplate mongoTemplate, MongoProperties mongoProperties) {
+    public Mongobee mongobee(MongoClient mongoClient, MongoTemplate mongoTemplate, MongoProperties mongoProperties, Environment environment) {
         log.debug("Configuring Mongobee");
         Mongobee mongobee = new Mongobee(mongoClient);
+        mongobee.setSpringEnvironment(environment);
         mongobee.setDbName(mongoProperties.getMongoClientDatabase());
         mongobee.setMongoTemplate(mongoTemplate);
         // package to scan for migrations
