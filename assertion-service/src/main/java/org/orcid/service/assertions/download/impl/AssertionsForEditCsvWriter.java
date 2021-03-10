a3d3c74e9b4a49e55569e36f1345577027a81a6b
George Nash <bobcaprice@hotmail.com>
2021-03-10 12:58:07 -0500

    Refactoring and tidying

--- assertion-service/src/main/java/org/orcid/service/assertions/download/impl/AssertionsForEditCsvWriter.java
+++ assertion-service/src/main/java/org/orcid/service/assertions/download/impl/AssertionsForEditCsvWriter.java
@@ -2,17 +2,13 @@ package org.orcid.service.assertions.download.impl;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.lang3.StringUtils;
 import org.orcid.domain.Assertion;
-import org.orcid.domain.OrcidRecord;
 import org.orcid.repository.AssertionsRepository;
-import org.orcid.service.OrcidRecordService;
 import org.orcid.service.UserService;
 import org.orcid.service.assertions.download.CsvWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,77 +27,66 @@ public class AssertionsForEditCsvWriter implements CsvWriter {
 	@Autowired
 	private AssertionsRepository assertionsRepository;
 
-	@Autowired
-	private OrcidRecordService orcidRecordService;
-
 	@Override
 	public String writeCsv() throws IOException {
 		String salesforceId = assertionsUserService.getLoggedInUserSalesforceId();
 		List<Assertion> assertions = assertionsRepository.findBySalesforceId(salesforceId, this.SORT);
 
 		StringBuffer buffer = new StringBuffer();
-		CSVPrinter csvPrinter = new CSVPrinter(buffer,
-				CSVFormat.DEFAULT.withHeader("email", "affiliation-section", "department-name", "role-title",
-						"start-date", "end-date", "org-name", "org-country", "org-city", "org-region",
-						"disambiguation-source", "disambiguated-organization-identifier", "external-id",
-						"external-id-type", "external-id-url", "url", "id"));
-		Map<String, OrcidRecord> orcidRecordMap = new HashMap<>();
-		List<String> elements;
+		CSVPrinter csvPrinter = getCSVPrinterWithHeaders(buffer);
+
 		for (Assertion a : assertions) {
-			elements = new ArrayList<String>();
-			elements.add(a.getEmail());
-
-			if (!orcidRecordMap.containsKey(a.getEmail())) {
-				OrcidRecord orcidRecord = orcidRecordService.findOneByEmail(a.getEmail())
-						.orElseThrow(() -> new IllegalArgumentException("Unable to find userInfo for " + a.getEmail()));
-				orcidRecordMap.put(a.getEmail(), orcidRecord);
-			}
-			elements.add(a.getAffiliationSection() == null ? "" : a.getAffiliationSection().name());
-			elements.add(a.getDepartmentName() == null ? "" : a.getDepartmentName());
-			elements.add(a.getRoleTitle() == null ? "" : a.getRoleTitle());
-
-			if (!StringUtils.isBlank(a.getStartYear())) {
-				String startDate = a.getStartYear();
-				if (!StringUtils.isBlank(a.getStartMonth())) {
-					startDate += '-' + a.getStartMonth();
-					if (!StringUtils.isBlank(a.getStartDay())) {
-						startDate += '-' + a.getStartDay();
-					}
-				}
-				elements.add(startDate);
-			} else {
-				elements.add(StringUtils.EMPTY);
-			}
-
-			if (!StringUtils.isBlank(a.getEndYear())) {
-				String endDate = a.getEndYear();
-				if (!StringUtils.isBlank(a.getEndMonth())) {
-					endDate += '-' + a.getEndMonth();
-					if (!StringUtils.isBlank(a.getEndDay())) {
-						endDate += '-' + a.getEndDay();
-					}
-				}
-				elements.add(endDate);
-			} else {
-				elements.add(StringUtils.EMPTY);
-			}
-
-			elements.add(StringUtils.isBlank(a.getOrgName()) ? "" : a.getOrgName());
-			elements.add(StringUtils.isBlank(a.getOrgCountry()) ? "" : a.getOrgCountry());
-			elements.add(StringUtils.isBlank(a.getOrgCity()) ? "" : a.getOrgCity());
-			elements.add(StringUtils.isBlank(a.getOrgRegion()) ? "" : a.getOrgRegion());
-			elements.add(StringUtils.isBlank(a.getDisambiguationSource()) ? "" : a.getDisambiguationSource());
-			elements.add(StringUtils.isBlank(a.getDisambiguatedOrgId()) ? "" : a.getDisambiguatedOrgId());
-			elements.add(StringUtils.isBlank(a.getExternalId()) ? "" : a.getExternalId());
-			elements.add(StringUtils.isBlank(a.getExternalIdType()) ? "" : a.getExternalIdType());
-			elements.add(StringUtils.isBlank(a.getExternalIdUrl()) ? "" : a.getExternalIdUrl());
-			elements.add(StringUtils.isBlank(a.getUrl()) ? "" : a.getUrl());
-			elements.add(a.getId());
-			csvPrinter.printRecord(elements);
+			List<String> values = getValues(a);
+			csvPrinter.printRecord(values);
 			csvPrinter.flush();
 		}
 		csvPrinter.close();
 		return buffer.toString();
+	}
+
+	private List<String> getValues(Assertion a) {
+		List<String> values = new ArrayList<String>();
+		values.add(a.getEmail());
+		values.add(a.getAffiliationSection() == null ? "" : a.getAffiliationSection().name());
+		values.add(a.getDepartmentName() == null ? "" : a.getDepartmentName());
+		values.add(a.getRoleTitle() == null ? "" : a.getRoleTitle());
+		values.add(getDateString(a.getStartYear(), a.getStartMonth(), a.getStartDay()));
+		values.add(getDateString(a.getEndYear(), a.getEndMonth(), a.getEndDay()));
+		values.add(StringUtils.isBlank(a.getOrgName()) ? "" : a.getOrgName());
+		values.add(StringUtils.isBlank(a.getOrgCountry()) ? "" : a.getOrgCountry());
+		values.add(StringUtils.isBlank(a.getOrgCity()) ? "" : a.getOrgCity());
+		values.add(StringUtils.isBlank(a.getOrgRegion()) ? "" : a.getOrgRegion());
+		values.add(StringUtils.isBlank(a.getDisambiguationSource()) ? "" : a.getDisambiguationSource());
+		values.add(StringUtils.isBlank(a.getDisambiguatedOrgId()) ? "" : a.getDisambiguatedOrgId());
+		values.add(StringUtils.isBlank(a.getExternalId()) ? "" : a.getExternalId());
+		values.add(StringUtils.isBlank(a.getExternalIdType()) ? "" : a.getExternalIdType());
+		values.add(StringUtils.isBlank(a.getExternalIdUrl()) ? "" : a.getExternalIdUrl());
+		values.add(StringUtils.isBlank(a.getUrl()) ? "" : a.getUrl());
+		values.add(a.getId());
+		return values;
+	}
+	
+	private String getDateString(String year, String month, String day) {
+		if (!StringUtils.isBlank(year)) {
+			String endDate = year;
+			if (!StringUtils.isBlank(month)) {
+				endDate += '-' + month;
+				if (!StringUtils.isBlank(day)) {
+					endDate += '-' + day;
+				}
+			}
+			return endDate;
+		} else {
+			return StringUtils.EMPTY;
+		}
+	}
+
+	private CSVPrinter getCSVPrinterWithHeaders(StringBuffer buffer) throws IOException {
+		return new CSVPrinter(buffer,
+				CSVFormat.DEFAULT.withHeader("email", "affiliation-section", "department-name", "role-title",
+						"start-date", "end-date", "org-name", "org-country", "org-city", "org-region",
+						"disambiguation-source", "disambiguated-organization-identifier", "external-id",
+						"external-id-type", "external-id-url", "url", "id"));
 	}
 
 }
