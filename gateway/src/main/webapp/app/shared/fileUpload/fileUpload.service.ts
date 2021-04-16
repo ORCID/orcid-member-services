1a9216203dda3421da7acdd49e80ac3ceb5a9572
George Nash <bobcaprice@hotmail.com>
2021-04-16 13:02:23 -0500

    UI changes for showing csv upload summary

--- gateway/src/main/webapp/app/shared/fileUpload/fileUpload.service.ts
+++ gateway/src/main/webapp/app/shared/fileUpload/fileUpload.service.ts
@@ -1,19 +1,18 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpRequest, HttpEvent} from '@angular/common/http';
-import {Observable} from 'rxjs/internal/Observable';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable()
 export class FileUploadService {
+  constructor(private http: HttpClient) {}
 
-	constructor(private http: HttpClient) {}
-
-    uploadFile(resourceUrl, file: File): Observable<HttpEvent<{}>> {
-        const formdata: FormData = new FormData();
-        formdata.append('file', file);
-        const req = new HttpRequest('POST', resourceUrl, formdata, {
-            reportProgress: true,
-            responseType: 'text'
-        });
-        return this.http.request(req);
-    }
+  uploadFile(resourceUrl, file: File, expectedResponseType): Observable<HttpEvent<{}>> {
+    const formdata: FormData = new FormData();
+    formdata.append('file', file);
+    const req = new HttpRequest('POST', resourceUrl, formdata, {
+      reportProgress: true,
+      responseType: expectedResponseType
+    });
+    return this.http.request(req);
+  }
 }
