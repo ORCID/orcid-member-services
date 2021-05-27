ef7c794825d1031d502b5d84e486f085ae4aa8de
George Nash <bobcaprice@hotmail.com>
2021-05-27 14:27:06 -0500

    Backend logic for chartio member report

---
+++ member-service/src/main/java/org/orcid/member/service/ReportService.java
@@ -0,0 +1,67 @@
+package org.orcid.member.service;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.orcid.member.config.ApplicationProperties;
+import org.orcid.member.service.reports.ReportInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class ReportService {
+
+	private static final String IAT_PARAM = "iat";
+
+	private static final String EXP_PARAM = "exp";
+
+	private static final String ORG_PARAM = "organization";
+
+	private static final String DASHBOARD_PARAM = "dashboard";
+
+	private static final String ENV_PARAM = "env";
+
+	private static final String SALESFORCE_ID_PARAM = "SF_ID";
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private ApplicationProperties applicationProperties;
+
+	public ReportInfo getMemberReportInfo() {
+		ReportInfo info = new ReportInfo();
+		info.setUrl(applicationProperties.getChartioMemberDashboardUrl());
+		info.setJwt(getJwt(Integer.valueOf(applicationProperties.getChartioMemberDashboardId())));
+		return info;
+	}
+
+	private String getJwt(int dashboard) {
+		long iat = Instant.now().toEpochMilli() / 1000;
+		long exp = iat + 900;
+
+		Map<String, Object> envMap = new HashMap<>();
+		envMap.put(SALESFORCE_ID_PARAM, getLoggedInSalesforceId());
+
+		Map<String, Object> claims = new HashMap<>();
+		claims.put(IAT_PARAM, iat);
+		claims.put(EXP_PARAM, exp);
+		claims.put(ORG_PARAM, Integer.valueOf(applicationProperties.getChartioOrgId()));
+		claims.put(DASHBOARD_PARAM, dashboard);
+		claims.put(ENV_PARAM, envMap);
+
+		return Jwts.builder().addClaims(claims)
+				.signWith(Keys.hmacShaKeyFor(applicationProperties.getChartioSecret().getBytes(StandardCharsets.UTF_8)))
+				.compact();
+	}
+
+	private String getLoggedInSalesforceId() {
+		return userService.getLoggedInUser().getSalesforceId();
+	}
+
+}
---
+++ member-service/src/main/java/org/orcid/member/service/reports/ReportInfo.java
@@ -0,0 +1,26 @@
+package org.orcid.member.service.reports;
+
+public class ReportInfo {
+
+	private String url;
+	
+	private String jwt;
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+
+	public void setJwt(String jwt) {
+		this.jwt = jwt;
+	}
+
+	
+}
