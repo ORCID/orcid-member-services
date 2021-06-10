a9808b7958d8c72c604c13bd9a553e90df402021
George Nash <bobcaprice@hotmail.com>
2021-06-09 19:50:34 -0500

    Changed report routing to pick up report type from url and send to component

--- gateway/src/main/webapp/app/report/report.route.ts
+++ gateway/src/main/webapp/app/report/report.route.ts
@@ -1,12 +1,26 @@
-import { Route } from '@angular/router';
-
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, Route } from '@angular/router';
 import { ReportComponent } from './';
+import { Observable, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ReportResolve implements Resolve<String> {
+  constructor() {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<String> {
+    const reportType = route.params['reportType'] ? route.params['reportType'] : null;
+    return of(reportType);
+  }
+}
 
 export const REPORT_ROUTE: Route = {
-  path: 'report',
+  path: 'report/:reportType',
   component: ReportComponent,
+  resolve: {
+    reportType: ReportResolve
+  },
   data: {
     authorities: [],
-    pageTitle: 'gatewayApp.report.title.string'
+    pageTitle: 'gatewayApp.report.member.title.string'
   }
 };
