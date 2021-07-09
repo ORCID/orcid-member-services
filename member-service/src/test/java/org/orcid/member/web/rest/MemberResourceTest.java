596619dcfbda98c44f5aa75ffdd7856afb418b4f
George Nash <bobcaprice@hotmail.com>
2021-07-09 17:20:02 +0200

    Formatting

--- member-service/src/test/java/org/orcid/member/web/rest/MemberResourceTest.java
+++ member-service/src/test/java/org/orcid/member/web/rest/MemberResourceTest.java
@@ -19,33 +19,33 @@ import org.orcid.member.validation.MemberValidation;
 import org.springframework.http.ResponseEntity;
 
 public class MemberResourceTest {
-	
-	@Mock
-	private MemberService memberService;
 
-	@InjectMocks
-	private MemberResource memberResource;
-	
-	@BeforeEach
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-	}
+    @Mock
+    private MemberService memberService;
 
-	@Test
-	public void testValidateMember() throws URISyntaxException, JSONException {
-		Mockito.when(memberService.validateMember(Mockito.any(Member.class))).thenReturn(getMemberValidation());
-		ResponseEntity<MemberValidation> validationResponse = memberResource.validateMember(new Member());
-		
-		// always 200, even if invalid member; the request to validate is valid
-		assertEquals(200, validationResponse.getStatusCodeValue()); 
-		assertTrue(validationResponse.getBody().isValid());
-		assertEquals(1, validationResponse.getBody().getErrors().size());
-	}
+    @InjectMocks
+    private MemberResource memberResource;
 
-	private MemberValidation getMemberValidation() {
-		MemberValidation validation = new MemberValidation();
-		validation.setValid(true);
-		validation.setErrors(Arrays.asList("some-error"));
-		return validation;
-	}
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testValidateMember() throws URISyntaxException, JSONException {
+        Mockito.when(memberService.validateMember(Mockito.any(Member.class))).thenReturn(getMemberValidation());
+        ResponseEntity<MemberValidation> validationResponse = memberResource.validateMember(new Member());
+
+        // always 200, even if invalid member; the request to validate is valid
+        assertEquals(200, validationResponse.getStatusCodeValue());
+        assertTrue(validationResponse.getBody().isValid());
+        assertEquals(1, validationResponse.getBody().getErrors().size());
+    }
+
+    private MemberValidation getMemberValidation() {
+        MemberValidation validation = new MemberValidation();
+        validation.setValid(true);
+        validation.setErrors(Arrays.asList("some-error"));
+        return validation;
+    }
 }
