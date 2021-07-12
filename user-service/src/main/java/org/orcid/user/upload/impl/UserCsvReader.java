177da601999052971649fa7474f1280f7b8f384d
George Nash <bobcaprice@hotmail.com>
2021-07-12 14:54:07 +0200

    Formatting

--- user-service/src/main/java/org/orcid/user/upload/impl/UserCsvReader.java
+++ user-service/src/main/java/org/orcid/user/upload/impl/UserCsvReader.java
@@ -30,79 +30,79 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserCsvReader implements UserUploadReader {
 
-	private static final Logger LOG = LoggerFactory.getLogger(UserCsvReader.class);
+    private static final Logger LOG = LoggerFactory.getLogger(UserCsvReader.class);
 
-	@Autowired
-	private UserValidator userValidator;
+    @Autowired
+    private UserValidator userValidator;
 
-	@Autowired
-	private MessageSource messageSource;
+    @Autowired
+    private MessageSource messageSource;
 
-	@Override
-	public UserUpload readUsersUpload(InputStream inputStream, User currentUser) throws IOException {
-		UserUpload upload = new UserUpload();
-		Instant now = Instant.now();
-		Map<String, UserDTO> mainContacts = new HashMap<>();
+    @Override
+    public UserUpload readUsersUpload(InputStream inputStream, User currentUser) throws IOException {
+        UserUpload upload = new UserUpload();
+        Instant now = Instant.now();
+        Map<String, UserDTO> mainContacts = new HashMap<>();
 
-		try (final Reader reader = new InputStreamReader(new BOMInputStream(inputStream), StandardCharsets.UTF_8);
-				final CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader())) {
+        try (final Reader reader = new InputStreamReader(new BOMInputStream(inputStream), StandardCharsets.UTF_8);
+                final CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader())) {
 
-			for (CSVRecord record : parser) {
-				try {
-					UserDTO userDTO = getUserDTO(record, now, currentUser.getLogin());
-					UserValidation userValidation = userValidator.validate(userDTO, currentUser);
-					boolean mainContactViolation = duplicateMainContact(userDTO, mainContacts);
-					if (userValidation.isValid() && !mainContactViolation) {
-						upload.getUserDTOs().add(userDTO);
-					} else {
-						for (String error : userValidation.getErrors()) {
-							upload.addError(record.getRecordNumber(), error);
-						}
+            for (CSVRecord record : parser) {
+                try {
+                    UserDTO userDTO = getUserDTO(record, now, currentUser.getLogin());
+                    UserValidation userValidation = userValidator.validate(userDTO, currentUser);
+                    boolean mainContactViolation = duplicateMainContact(userDTO, mainContacts);
+                    if (userValidation.isValid() && !mainContactViolation) {
+                        upload.getUserDTOs().add(userDTO);
+                    } else {
+                        for (String error : userValidation.getErrors()) {
+                            upload.addError(record.getRecordNumber(), error);
+                        }
 
-						if (mainContactViolation) {
-							upload.addError(record.getRecordNumber(), getError("multipleOrgOwners", null, currentUser));
-						}
-					}
-				} catch (Exception e) {
-					LOG.info("CSV upload error found for record number {}", record.getRecordNumber());
-					upload.addError(record.getRecordNumber(), e.getMessage());
-				}
-			}
-		}
-		return upload;
-	}
+                        if (mainContactViolation) {
+                            upload.addError(record.getRecordNumber(), getError("multipleOrgOwners", null, currentUser));
+                        }
+                    }
+                } catch (Exception e) {
+                    LOG.info("CSV upload error found for record number {}", record.getRecordNumber());
+                    upload.addError(record.getRecordNumber(), e.getMessage());
+                }
+            }
+        }
+        return upload;
+    }
 
-	private boolean duplicateMainContact(UserDTO userDTO, Map<String, UserDTO> mainContacts) {
-		UserDTO existingMainContact = mainContacts.get(userDTO.getSalesforceId());
-		if (existingMainContact != null && !existingMainContact.getEmail().equals(userDTO.getEmail())) {
-			return true;
-		}
+    private boolean duplicateMainContact(UserDTO userDTO, Map<String, UserDTO> mainContacts) {
+        UserDTO existingMainContact = mainContacts.get(userDTO.getSalesforceId());
+        if (existingMainContact != null && !existingMainContact.getEmail().equals(userDTO.getEmail())) {
+            return true;
+        }
 
-		mainContacts.put(userDTO.getSalesforceId(), userDTO);
-		return false;
-	}
+        mainContacts.put(userDTO.getSalesforceId(), userDTO);
+        return false;
+    }
 
-	private UserDTO getUserDTO(CSVRecord record, Instant now, String createdBy) {
-		UserDTO u = new UserDTO();
-		u.setLogin(record.get("email"));
-		u.setFirstName(record.get("firstName"));
-		u.setLastName(record.get("lastName"));
-		u.setSalesforceId(record.get("salesforceId"));
-		u.setPassword(RandomStringUtils.randomAlphanumeric(10));
-		u.setSalesforceId(record.get("salesforceId"));
-		u.setCreatedBy(createdBy);
-		u.setCreatedDate(now);
-		u.setLastModifiedBy(createdBy);
-		u.setLastModifiedDate(now);
-		u.setEmail(record.get("email"));
-		u.setMainContact(new Boolean(record.get("mainContact")));
-		u.setLangKey("en");
-		return u;
-	}
+    private UserDTO getUserDTO(CSVRecord record, Instant now, String createdBy) {
+        UserDTO u = new UserDTO();
+        u.setLogin(record.get("email"));
+        u.setFirstName(record.get("firstName"));
+        u.setLastName(record.get("lastName"));
+        u.setSalesforceId(record.get("salesforceId"));
+        u.setPassword(RandomStringUtils.randomAlphanumeric(10));
+        u.setSalesforceId(record.get("salesforceId"));
+        u.setCreatedBy(createdBy);
+        u.setCreatedDate(now);
+        u.setLastModifiedBy(createdBy);
+        u.setLastModifiedDate(now);
+        u.setEmail(record.get("email"));
+        u.setMainContact(new Boolean(record.get("mainContact")));
+        u.setLangKey("en");
+        return u;
+    }
 
-	private String getError(String code, String arg, User user) {
-		return messageSource.getMessage("user.validation.error." + code, arg != null ? new Object[] { arg } : null,
-				Locale.forLanguageTag(user.getLangKey()));
-	}
+    private String getError(String code, String arg, User user) {
+        return messageSource.getMessage("user.validation.error." + code, arg != null ? new Object[] { arg } : null,
+                Locale.forLanguageTag(user.getLangKey()));
+    }
 
 }
--- user-service/src/test/java/org/orcid/user/upload/impl/UserCsvReaderTest.java
+++ user-service/src/test/java/org/orcid/user/upload/impl/UserCsvReaderTest.java
@@ -26,98 +26,98 @@ import org.springframework.context.MessageSource;
 
 class UserCsvReaderTest {
 
-	@Mock
-	private UserValidator userValidator;
+    @Mock
+    private UserValidator userValidator;
 
-	@Mock
-	private MessageSource messageSource;
+    @Mock
+    private MessageSource messageSource;
 
-	@InjectMocks
-	private UserCsvReader reader;
+    @InjectMocks
+    private UserCsvReader reader;
 
-	@BeforeEach
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-	}
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
-	@Test
-	void testReadUsersUpload() throws IOException {
-		Mockito.when(userValidator.validate(Mockito.any(UserDTO.class), Mockito.any(User.class)))
-				.thenReturn(getUserValidation(new ArrayList<>()));
+    @Test
+    void testReadUsersUpload() throws IOException {
+        Mockito.when(userValidator.validate(Mockito.any(UserDTO.class), Mockito.any(User.class)))
+                .thenReturn(getUserValidation(new ArrayList<>()));
 
-		InputStream inputStream = getClass().getResourceAsStream("/users.csv");
-		UserUpload upload = reader.readUsersUpload(inputStream, getUser("en"));
-		assertEquals(3, upload.getUserDTOs().size());
+        InputStream inputStream = getClass().getResourceAsStream("/users.csv");
+        UserUpload upload = reader.readUsersUpload(inputStream, getUser("en"));
+        assertEquals(3, upload.getUserDTOs().size());
 
-		UserDTO userDTO1 = upload.getUserDTOs().get(0);
-		UserDTO userDTO2 = upload.getUserDTOs().get(1);
-		UserDTO userDTO3 = upload.getUserDTOs().get(2);
+        UserDTO userDTO1 = upload.getUserDTOs().get(0);
+        UserDTO userDTO2 = upload.getUserDTOs().get(1);
+        UserDTO userDTO3 = upload.getUserDTOs().get(2);
 
-		assertEquals("1@user.com", userDTO1.getLogin());
-		assertEquals("2@user.com", userDTO2.getLogin());
-		assertEquals("3@user.com", userDTO3.getLogin());
+        assertEquals("1@user.com", userDTO1.getLogin());
+        assertEquals("2@user.com", userDTO2.getLogin());
+        assertEquals("3@user.com", userDTO3.getLogin());
 
-		assertEquals("Angel", userDTO1.getFirstName());
-		assertEquals("Leonardo", userDTO2.getFirstName());
-		assertEquals("Daniel", userDTO3.getFirstName());
+        assertEquals("Angel", userDTO1.getFirstName());
+        assertEquals("Leonardo", userDTO2.getFirstName());
+        assertEquals("Daniel", userDTO3.getFirstName());
 
-		assertEquals("Montenegro", userDTO1.getLastName());
-		assertEquals("Mendoza", userDTO2.getLastName());
-		assertEquals("Palafox", userDTO3.getLastName());
+        assertEquals("Montenegro", userDTO1.getLastName());
+        assertEquals("Mendoza", userDTO2.getLastName());
+        assertEquals("Palafox", userDTO3.getLastName());
 
-		assertEquals("sssalesforceid1", userDTO1.getSalesforceId());
-		assertEquals("salesforceid3", userDTO2.getSalesforceId());
-		assertEquals("salesforceid2", userDTO3.getSalesforceId());
-	}
+        assertEquals("sssalesforceid1", userDTO1.getSalesforceId());
+        assertEquals("salesforceid3", userDTO2.getSalesforceId());
+        assertEquals("salesforceid2", userDTO3.getSalesforceId());
+    }
 
-	@Test
-	void testReadUsersUploadInvalidEmails() throws IOException, JSONException {
-		Mockito.when(userValidator.validate(Mockito.any(UserDTO.class), Mockito.any(User.class)))
-				.thenReturn(getUserValidation(Arrays.asList("some-error")));
+    @Test
+    void testReadUsersUploadInvalidEmails() throws IOException, JSONException {
+        Mockito.when(userValidator.validate(Mockito.any(UserDTO.class), Mockito.any(User.class)))
+                .thenReturn(getUserValidation(Arrays.asList("some-error")));
 
-		InputStream inputStream = getClass().getResourceAsStream("/users.csv");
-		UserUpload upload = reader.readUsersUpload(inputStream, getUser("en"));
-		assertEquals(0, upload.getUserDTOs().size());
-		assertEquals(3, upload.getErrors().length());
+        InputStream inputStream = getClass().getResourceAsStream("/users.csv");
+        UserUpload upload = reader.readUsersUpload(inputStream, getUser("en"));
+        assertEquals(0, upload.getUserDTOs().size());
+        assertEquals(3, upload.getErrors().length());
 
-		assertTrue(upload.getErrors().get(0).toString().contains("some-error"));
-		assertTrue(upload.getErrors().get(1).toString().contains("some-error"));
-		assertTrue(upload.getErrors().get(2).toString().contains("some-error"));
-	}
+        assertTrue(upload.getErrors().get(0).toString().contains("some-error"));
+        assertTrue(upload.getErrors().get(1).toString().contains("some-error"));
+        assertTrue(upload.getErrors().get(2).toString().contains("some-error"));
+    }
 
-	@Test
-	void testReadUsersUploadDuplicateMainContactsWithinSameSpreadsheet() throws IOException, JSONException {
-		Mockito.when(userValidator.validate(Mockito.any(UserDTO.class), Mockito.any(User.class)))
-				.thenReturn(getUserValidation(new ArrayList<>()));
-		Mockito.when(messageSource.getMessage(Mockito.eq("user.validation.error.multipleOrgOwners"), Mockito.any(),
-				Mockito.eq(Locale.ENGLISH))).thenReturn("some-value");
+    @Test
+    void testReadUsersUploadDuplicateMainContactsWithinSameSpreadsheet() throws IOException, JSONException {
+        Mockito.when(userValidator.validate(Mockito.any(UserDTO.class), Mockito.any(User.class)))
+                .thenReturn(getUserValidation(new ArrayList<>()));
+        Mockito.when(messageSource.getMessage(Mockito.eq("user.validation.error.multipleOrgOwners"), Mockito.any(),
+                Mockito.eq(Locale.ENGLISH))).thenReturn("some-value");
 
-		InputStream inputStream = getClass().getResourceAsStream("/users-with-multiple-main-contacts.csv");
-		UserUpload upload = reader.readUsersUpload(inputStream, getUser("en"));
-		assertEquals(3, upload.getUserDTOs().size());
-		assertEquals(1, upload.getErrors().length());
+        InputStream inputStream = getClass().getResourceAsStream("/users-with-multiple-main-contacts.csv");
+        UserUpload upload = reader.readUsersUpload(inputStream, getUser("en"));
+        assertEquals(3, upload.getUserDTOs().size());
+        assertEquals(1, upload.getErrors().length());
 
-		assertTrue(upload.getErrors().get(0).toString().contains("some-value"));
+        assertTrue(upload.getErrors().get(0).toString().contains("some-value"));
 
-		Mockito.verify(messageSource, Mockito.times(1)).getMessage(
-				Mockito.eq("user.validation.error.multipleOrgOwners"), Mockito.any(), Mockito.eq(Locale.ENGLISH));
-	}
+        Mockito.verify(messageSource, Mockito.times(1)).getMessage(
+                Mockito.eq("user.validation.error.multipleOrgOwners"), Mockito.any(), Mockito.eq(Locale.ENGLISH));
+    }
 
-	private UserValidation getUserValidation(List<String> errors) {
-		UserValidation validation = new UserValidation();
-		validation.setValid(errors.isEmpty());
-		validation.setErrors(errors);
-		return validation;
-	}
+    private UserValidation getUserValidation(List<String> errors) {
+        UserValidation validation = new UserValidation();
+        validation.setValid(errors.isEmpty());
+        validation.setErrors(errors);
+        return validation;
+    }
 
-	private User getUser(String langKey) {
-		User user = new User();
-		user.setId("some-id");
-		user.setLangKey(langKey);
-		user.setLogin("something@orcid.org");
-		user.setLoginAs("something@orcid.org");
-		user.setSalesforceId("something");
-		return user;
-	}
+    private User getUser(String langKey) {
+        User user = new User();
+        user.setId("some-id");
+        user.setLangKey(langKey);
+        user.setLogin("something@orcid.org");
+        user.setLoginAs("something@orcid.org");
+        user.setSalesforceId("something");
+        return user;
+    }
 
 }
