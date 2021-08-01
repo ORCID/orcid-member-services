d675aea603b344a4323b3bf0f7a74b26f496c088
George Nash <bobcaprice@hotmail.com>
2021-08-01 15:40:11 +0100

    New mailgun api client

---
+++ user-service/src/main/java/org/orcid/user/service/mail/client/MailClient.java
@@ -0,0 +1,9 @@
+package org.orcid.user.service.mail.client;
+
+import org.orcid.user.service.mail.MailException;
+
+public interface MailClient {
+	
+	void sendMail(String to, String subject, String html) throws MailException;
+
+}
