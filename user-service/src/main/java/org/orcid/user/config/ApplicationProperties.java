f0e5c2e6a21c4dd6deca92f9cd9c6513b62e8816
George Nash <bobcaprice@hotmail.com>
2021-08-01 15:41:44 +0100

    Mail service now uses mailgun api client

--- user-service/src/main/java/org/orcid/user/config/ApplicationProperties.java
+++ user-service/src/main/java/org/orcid/user/config/ApplicationProperties.java
@@ -2,13 +2,77 @@ package org.orcid.user.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-/**
- * Properties specific to Oauth 2 Service.
- * <p>
- * Properties are configured in the {@code application.yml} file.
- * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
- */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
 
+    private String mailDomain;
+    
+    private String mailApiKey;
+    
+    private String mailApiUrl;
+    
+    private String mailFromAddress;
+    
+    private String mailFromName;
+    
+    private boolean mailTestMode;
+    
+    private String baseUrl;
+
+    public String getMailDomain() {
+        return mailDomain;
+    }
+
+    public void setMailDomain(String mailDomain) {
+        this.mailDomain = mailDomain;
+    }
+
+    public String getMailApiKey() {
+        return mailApiKey;
+    }
+
+    public void setMailApiKey(String mailApiKey) {
+        this.mailApiKey = mailApiKey;
+    }
+
+    public String getMailFromAddress() {
+        return mailFromAddress;
+    }
+
+    public void setMailFromAddress(String mailFromAddress) {
+        this.mailFromAddress = mailFromAddress;
+    }
+
+    public String getMailFromName() {
+        return mailFromName;
+    }
+
+    public void setMailFromName(String mailFromName) {
+        this.mailFromName = mailFromName;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getMailApiUrl() {
+        return mailApiUrl;
+    }
+
+    public void setMailApiUrl(String mailApiUrl) {
+        this.mailApiUrl = mailApiUrl;
+    }
+
+	public boolean isMailTestMode() {
+		return mailTestMode;
+	}
+
+	public void setMailTestMode(boolean mailTestMode) {
+		this.mailTestMode = mailTestMode;
+	}
+    
 }
--- user-service/src/main/java/org/orcid/user/service/MailService.java
+++ user-service/src/main/java/org/orcid/user/service/MailService.java
@@ -1,17 +1,16 @@
 package org.orcid.user.service;
 
-import io.github.jhipster.config.JHipsterProperties;
-
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.util.Locale;
-import javax.mail.internet.MimeMessage;
 
+import org.apache.http.client.ClientProtocolException;
+import org.orcid.user.config.ApplicationProperties;
 import org.orcid.user.domain.User;
+import org.orcid.user.service.mail.MailException;
+import org.orcid.user.service.mail.client.impl.MailgunClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
@@ -25,113 +24,106 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 @Service
 public class MailService {
 
-    private final Logger log = LoggerFactory.getLogger(MailService.class);
+	private final Logger LOGGER = LoggerFactory.getLogger(MailService.class);
 
-    private static final String USER = "user";
+	private static final String USER = "user";
 
-    private static final String BASE_URL = "baseUrl";
+	private static final String BASE_URL = "baseUrl";
 
-    private static final String MEMBER = "member";
+	private static final String MEMBER = "member";
 
-    private static final String EMAIL = "email";
+	private static final String EMAIL = "email";
 
-    private static final String INFO_EMAIL = "infoEmail";
+	private static final String INFO_EMAIL = "infoEmail";
 
-    private final JHipsterProperties jHipsterProperties;
+	private final MessageSource messageSource;
 
-    private final JavaMailSender javaMailSender;
+	private final SpringTemplateEngine templateEngine;
 
-    private final MessageSource messageSource;
+	private ApplicationProperties applicationProperties;
 
-    private final SpringTemplateEngine templateEngine;
+	private MailgunClient mailgunClient;
 
-    public MailService(JHipsterProperties jHipsterProperties, JavaMailSender javaMailSender,
-            MessageSource messageSource, SpringTemplateEngine templateEngine) {
+	public MailService(ApplicationProperties applicationProperties, MessageSource messageSource,
+			SpringTemplateEngine templateEngine, MailgunClient mailgunClient) {
+		this.applicationProperties = applicationProperties;
+		this.messageSource = messageSource;
+		this.templateEngine = templateEngine;
+		this.mailgunClient = mailgunClient;
+	}
 
-        this.jHipsterProperties = jHipsterProperties;
-        this.javaMailSender = javaMailSender;
-        this.messageSource = messageSource;
-        this.templateEngine = templateEngine;
-    }
+	@Async
+	public void sendEmail(String to, String subject, String content) throws ClientProtocolException, IOException {
+		try {
+			mailgunClient.sendMail(to, subject, content);
+		} catch (MailException e) {
+			LOGGER.warn("Error sending email to {}", to, content);
+		}
+	}
 
-    @Async
-    public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
-        log.debug("Send email[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
-            isMultipart, isHtml, to, subject, content);
+	@Async
+	public void sendEmailFromTemplate(User user, String templateName, String titleKey) {
+		Locale locale = Locale.forLanguageTag(user.getLangKey());
+		Context context = new Context(locale);
+		context.setVariable(USER, user);
+		context.setVariable(BASE_URL, applicationProperties.getBaseUrl());
+		String content = templateEngine.process(templateName, context);
+		String subject = messageSource.getMessage(titleKey, null, locale);
+		try {
+			sendEmail(user.getEmail(), subject, content);
+		} catch (IOException e) {
+			LOGGER.warn("Mail sending failure: {}", e.getMessage());
+		}
+	}
 
-        // Prepare message using a Spring helper
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
-            message.setTo(to);
-            message.setFrom(jHipsterProperties.getMail().getFrom());
-            message.setSubject(subject);
-            message.setText(content, isHtml);
-            javaMailSender.send(mimeMessage);
-            log.debug("Sent email to User '{}'", to);
-        } catch (Exception e) {
-            if (log.isDebugEnabled()) {
-                log.warn("Email could not be sent to user '{}'", to, e);
-            } else {
-                log.warn("Email could not be sent to user '{}': {}", to, e.getMessage());
-            }
-        }
-    }
+	@Async
+	public void sendEmailFromTemplateMemberInfo(User user, String member, String templateName, String titleKey) {
+		Locale locale = Locale.forLanguageTag(user.getLangKey());
+		Context context = new Context(locale);
+		String baseUrl = applicationProperties.getBaseUrl();
+		String infoEmail = "info@member-portal.orcid.org";
+		if ("https://member-portal.qa.orcid.org".equals(baseUrl)) {
+			infoEmail = "info@member-portal.qa.orcid.org";
+		} else if ("https://member-portal.sandbox.orcid.org".equals(baseUrl)) {
+			infoEmail = "info@member-portal.sandbox.orcid.org";
+		}
 
-    @Async
-    public void sendEmailFromTemplate(User user, String templateName, String titleKey) {
-        Locale locale = Locale.forLanguageTag(user.getLangKey());
-        Context context = new Context(locale);
-        context.setVariable(USER, user);
-        context.setVariable(BASE_URL, jHipsterProperties.getMail().getBaseUrl());
-        String content = templateEngine.process(templateName, context);
-        String subject = messageSource.getMessage(titleKey, null, locale);
-        sendEmail(user.getEmail(), subject, content, false, true);
-    }
+		context.setVariable(MEMBER, member + ".");
+		context.setVariable(USER, user);
+		context.setVariable(BASE_URL, baseUrl);
+		context.setVariable(INFO_EMAIL, infoEmail);
+		context.setVariable(EMAIL, applicationProperties.getMailFromAddress());
+		String content = templateEngine.process(templateName, context);
+		String subject = messageSource.getMessage(titleKey, null, locale);
 
-    @Async
-    public void sendEmailFromTemplateMemberInfo(User user, String member, String templateName, String titleKey) {
-        Locale locale = Locale.forLanguageTag(user.getLangKey());
-        Context context = new Context(locale);
-        String baseUrl = jHipsterProperties.getMail().getBaseUrl();
-        String infoEmail = "info@member-portal.orcid.org";
-        if ("https://member-portal.qa.orcid.org".equals(baseUrl)) {
-            infoEmail = "info@member-portal.qa.orcid.org";
-        } else if ("https://member-portal.sandbox.orcid.org".equals(baseUrl)) {
-            infoEmail = "info@member-portal.sandbox.orcid.org";
-        }
+		try {
+			sendEmail(user.getEmail(), subject, content);
+		} catch (IOException e) {
+			LOGGER.warn("Mail sending failure: {}", e.getMessage());
+		}
+	}
 
-        context.setVariable(MEMBER, member + ".");
-        context.setVariable(USER, user);
-        context.setVariable(BASE_URL, baseUrl);
-        context.setVariable(INFO_EMAIL, infoEmail);
-        context.setVariable(EMAIL, jHipsterProperties.getMail().getFrom());
-        String content = templateEngine.process(templateName, context);
-        String subject = messageSource.getMessage(titleKey, null, locale);
-        sendEmail(user.getEmail(), subject, content, false, true);
-    }
+	@Async
+	public void sendActivationEmail(User user) {
+		LOGGER.debug("Sending activation email to '{}'", user.getEmail());
+		sendEmailFromTemplate(user, "mail/activationEmail", "email.activation.title");
+	}
 
-    @Async
-    public void sendActivationEmail(User user) {
-        log.debug("Sending activation email to '{}'", user.getEmail());
-        sendEmailFromTemplate(user, "mail/activationEmail", "email.activation.title");
-    }
+	@Async
+	public void sendCreationEmail(User user) {
+		LOGGER.debug("Sending creation email to '{}'", user.getEmail());
+		sendEmailFromTemplate(user, "mail/creationEmail", "email.activation.title");
+	}
 
-    @Async
-    public void sendCreationEmail(User user) {
-        log.debug("Sending creation email to '{}'", user.getEmail());
-        sendEmailFromTemplate(user, "mail/creationEmail", "email.activation.title");
-    }
+	@Async
+	public void sendPasswordResetMail(User user) {
+		LOGGER.debug("Sending password reset email to '{}'", user.getEmail());
+		sendEmailFromTemplate(user, "mail/passwordResetEmail", "email.reset.title");
+	}
 
-    @Async
-    public void sendPasswordResetMail(User user) {
-        log.debug("Sending password reset email to '{}'", user.getEmail());
-        sendEmailFromTemplate(user, "mail/passwordResetEmail", "email.reset.title");
-    }
-
-    @Async
-    public void sendOrganizationOwnerChangedMail(User user, String member) {
-        log.debug("Sending organization owner changed email to '{}'", user.getEmail());
-        sendEmailFromTemplateMemberInfo(user, member,"mail/organizationOwnerChanged", "email.organization.title");
-    }
+	@Async
+	public void sendOrganizationOwnerChangedMail(User user, String member) {
+		LOGGER.debug("Sending organization owner changed email to '{}'", user.getEmail());
+		sendEmailFromTemplateMemberInfo(user, member, "mail/organizationOwnerChanged", "email.organization.title");
+	}
 }
