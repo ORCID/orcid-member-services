6126fd461039b9aaa17c1b2a82bcda8fa9c7f6b9
George Nash <bobcaprice@hotmail.com>
2021-08-12 18:03:54 +0100

    Navigate to users list after updating org owner as well as normal saves

--- gateway/src/main/webapp/app/entities/MSUserService/ms-users/ms-user-update.component.ts
+++ gateway/src/main/webapp/app/entities/MSUserService/ms-users/ms-user-update.component.ts
@@ -300,13 +300,13 @@ export class MSUserUpdateComponent implements OnInit {
 
   protected onSaveSuccessOwnershipChange() {
     this.isSaving = false;
-    window.location.href = SERVER_API_URL;
+    this.navigateToUsersList();
     this.alertService.success('userServiceApp.user.created.string');
   }
 
   protected onUpdateSuccessOwnershipChange() {
     this.isSaving = false;
-    window.location.href = SERVER_API_URL;
+    this.navigateToUsersList();
     this.alertService.success('userServiceApp.user.updated.string');
   }
 
