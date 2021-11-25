64b4432f189721b338dbb4877fa47b481af16f9a
George Nash <bobcaprice@hotmail.com>
2021-11-25 14:05:56 -0500

    Member client for assertion service

---
+++ assertion-service/src/main/java/org/orcid/memberportal/service/assertion/domain/AssertionServiceMember.java
@@ -0,0 +1,80 @@
+package org.orcid.memberportal.service.assertion.domain;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class AssertionServiceMember {
+
+    @Field("client_id")
+    private String clientId;
+
+    @Field("salesforce_id")
+    private String salesforceId;
+
+    @Field("parent_salesforce_id")
+    private String parentSalesforceId;
+
+    @Field("client_name")
+    private String clientName;
+
+    @Field("assertion_service_enabled")
+    private Boolean assertionServiceEnabled;
+
+    @Field("superadmin_enabled")
+    private Boolean superadminEnabled;
+
+    @Field("is_consortium_lead")
+    private Boolean isConsortiumLead;
+
+    public Boolean getIsConsortiumLead() {
+        return isConsortiumLead;
+    }
+
+    public void setIsConsortiumLead(Boolean isConsortiumLead) {
+        this.isConsortiumLead = isConsortiumLead;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getSalesforceId() {
+        return salesforceId;
+    }
+
+    public void setSalesforceId(String salesforceId) {
+        this.salesforceId = salesforceId;
+    }
+
+    public String getParentSalesforceId() {
+        return parentSalesforceId;
+    }
+
+    public void setParentSalesforceId(String parentSalesforceId) {
+        this.parentSalesforceId = parentSalesforceId;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public Boolean getAssertionServiceEnabled() {
+        return assertionServiceEnabled;
+    }
+
+    public void setAssertionServiceEnabled(Boolean assertionServiceEnabled) {
+        this.assertionServiceEnabled = assertionServiceEnabled;
+    }
+
+    public Boolean getSuperadminEnabled() {
+        return superadminEnabled;
+    }
+
+}
---
+++ assertion-service/src/main/java/org/orcid/memberportal/service/assertion/services/MemberService.java
@@ -0,0 +1,26 @@
+package org.orcid.memberportal.service.assertion.services;
+
+import org.orcid.memberportal.service.assertion.client.MemberServiceClient;
+import org.orcid.memberportal.service.assertion.domain.AssertionServiceMember;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberServiceClient memberServiceClient;
+
+    public String getMemberName(String salesforceId) {
+        ResponseEntity<AssertionServiceMember> response = memberServiceClient.getMember(salesforceId);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody().getClientName();
+        }
+        if (response.getStatusCodeValue() == 404) {
+            return null;
+        }
+        throw new RuntimeException("Error contacting member service");
+    }
+
+}
