f2e05dcd773ea2576e61b8749111e1357fd118ba
George Nash <bobcaprice@hotmail.com>
2022-02-03 11:23:25 +0000

    Added chinese label

--- gateway/src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
+++ gateway/src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -8,7 +8,7 @@ export class FindLanguageFromKeyPipe implements PipeTransform {
     fr: { name: 'Français' },
     ja: { name: '日本語' },
     zh_TW: { name: '繁體中文' },
-    zh_CN: { name: 'zh-CN' },
+    zh_CN: { name: '简体中文' },
     cs: { name: 'Čeština' },
     it: { name: 'Italiano' },
     ko: { name: '한국어' },
