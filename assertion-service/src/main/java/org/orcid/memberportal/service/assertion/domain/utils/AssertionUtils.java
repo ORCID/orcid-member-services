af316247b34b384a3fe58b9883e0b09e3c2180c0
George Nash <bobcaprice@hotmail.com>
2022-04-13 10:35:08 +0100

    Status now set after events rather than calculated based on assertion data

--- assertion-service/src/main/java/org/orcid/memberportal/service/assertion/domain/utils/AssertionUtils.java
+++ assertion-service/src/main/java/org/orcid/memberportal/service/assertion/domain/utils/AssertionUtils.java
@@ -1,63 +1,12 @@
 package org.orcid.memberportal.service.assertion.domain.utils;
 
 import org.apache.commons.lang3.StringUtils;
-import org.json.JSONObject;
-import org.orcid.memberportal.service.assertion.domain.Assertion;
-import org.orcid.memberportal.service.assertion.domain.OrcidRecord;
-import org.orcid.memberportal.service.assertion.domain.enumeration.AssertionStatus;
 
 public class AssertionUtils {
     private static final String GRID_BASE_URL = "https://www.grid.ac/";
     private static final String GRID_BASE_URL_INSTITUTES = "https://www.grid.ac/institutes/";
     private static final String GRID_BASE_URL_ALT = "https://grid.ac/";
     private static final String GRID_BASE_URL_INSTITUTES_ALT = "https://grid.ac/institutes/";
-
-    public static String getAssertionStatus(Assertion assertion, OrcidRecord orcidRecord, String defaultError) {
-        if (orcidRecord.getRevokedDate(assertion.getSalesforceId()) != null) {
-            return AssertionStatus.USER_REVOKED_ACCESS.name();
-        }
-        if (orcidRecord.getDeniedDate(assertion.getSalesforceId()) != null) {
-            return AssertionStatus.USER_DENIED_ACCESS.name();
-        }
-        
-        if (assertion.getOrcidError() != null && !assertionModifiedSinceLastSyncAttempt(assertion)) {
-            return getErrorStatus(assertion, defaultError);
-        }
-
-        if (assertionModifiedSinceLastSyncAttempt(assertion)) {
-            return AssertionStatus.PENDING_RETRY.name();
-        }
-
-        if (StringUtils.isBlank(assertion.getPutCode())) {
-            return AssertionStatus.PENDING.name();
-        }
-
-        return AssertionStatus.IN_ORCID.name();
-    }
-
-    private static String getErrorStatus(Assertion assertion, String defaultError) {
-        JSONObject json = new JSONObject(assertion.getOrcidError());
-        int statusCode = json.getInt("statusCode");
-        String errorMessage = json.getString("error");
-        switch (statusCode) {
-        case 404:
-            return AssertionStatus.USER_DELETED_FROM_ORCID.name();
-        case 401:
-            return AssertionStatus.USER_REVOKED_ACCESS.name();
-        case 400:
-            if (errorMessage.contains("invalid_scope")) {
-                return AssertionStatus.USER_REVOKED_ACCESS.name();
-            } else {
-                return defaultError;
-            }
-        default:
-            return defaultError;
-        }
-    }
-
-    private static boolean assertionModifiedSinceLastSyncAttempt(Assertion assertion) {
-        return assertion.getModified() != null && assertion.getLastSyncAttempt() != null && assertion.getModified().isAfter(assertion.getLastSyncAttempt());
-    }
 
     public static String stripGridURL(String gridIdentifier) {
         if (!StringUtils.isBlank(gridIdentifier)) {
