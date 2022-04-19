5361ca71b777c1153f06511a96f7f2a0d873a559
andrej romanov <50377758+auumgn@users.noreply.github.com>
2022-04-19 18:34:03 +0300

    Revert "add csv related data"
    This reverts commit 93aee5592fd490bcf6128c183c8395e4040d2b0c.

--- gateway/cypress/integration/retention/ui-header-user.js
+++ gateway/cypress/integration/retention/ui-header-user.js
@@ -3,15 +3,12 @@ import data from '../../fixtures/test-data.json';
 import credentials from '../../fixtures/credentials.json';
 
 describe('Test header for appropriate drop down menus', () => {
-  afterEach(() => {
-    cy.programmaticSignout();
-  })
-
   it('Test user account', function() {
     cy.programmaticSignin(data.populatedMember.users.user.email, credentials.password);
     cy.visit('/');
     cy.get('#admin-menu').should('not.exist');
     cy.get('#entity-menu').should('exist')
+    cy.programmaticSignout()
   });
 
   it('Test owner account', function() {
@@ -21,6 +18,7 @@ describe('Test header for appropriate drop down menus', () => {
     cy.get('#entity-menu').should('exist');
     cy.get('a').filter('[href="/ms-user"]').should('exist');
     cy.get('a').filter('[href="/ms-member"]').should('not.exist');
+    cy.programmaticSignout();
   });
 
   it('Test admin account', function() {
@@ -30,5 +28,6 @@ describe('Test header for appropriate drop down menus', () => {
     cy.get('#entity-menu').should('exist')
     cy.get('a').filter('[href="/ms-user"]').should('exist');
     cy.get('a').filter('[href="/ms-member"]').should('exist');
+    cy.programmaticSignout()
   });
 });
