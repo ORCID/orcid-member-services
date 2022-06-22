d6d6a27bf990eabe391a45dbf464612c6ac5a23f
andrej romanov <50377758+auumgn@users.noreply.github.com>
2022-06-22 13:38:57 +0300

    add models and display draft data in ui

--- gateway/src/main/webapp/app/home/home.component.ts
+++ gateway/src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { JhiEventManager } from 'ng-jhipster';
-import { share } from 'rxjs/operators';
-import { AccountService, UserService } from 'app/core';
+import { AccountService } from 'app/core';
 import { IMSUser } from 'app/shared/model/user.model';
+import { ISFMemberData } from 'app/shared/model/salesforce.member.data.model';
 import { MSMemberService } from 'app/entities/member';
 
 @Component({
@@ -11,29 +11,26 @@ import { MSMemberService } from 'app/entities/member';
 })
 export class HomeComponent implements OnInit {
   account: IMSUser;
+  memberData: ISFMemberData;
 
   constructor(private accountService: AccountService, private eventManager: JhiEventManager, private memberService: MSMemberService) {}
 
   ngOnInit() {
+    this.initializeAccount();
+    this.eventManager.subscribe('authenticationSuccess', message => {
+      this.initializeAccount();
+    });
+  }
+
+  initializeAccount() {
     this.accountService.identity().then((account: IMSUser) => {
       this.account = account;
-      const salesforceId = this.accountService.getSalesforceId();
-      console.log(salesforceId);
       this.memberService
         .getMember()
         .pipe()
         .subscribe(res => {
-          console.log(res);
+          this.memberData = res;
         });
-    });
-    this.registerAuthenticationSuccess();
-  }
-
-  registerAuthenticationSuccess() {
-    this.eventManager.subscribe('authenticationSuccess', message => {
-      this.accountService.identity().then(account => {
-        this.account = account;
-      });
     });
   }
 
