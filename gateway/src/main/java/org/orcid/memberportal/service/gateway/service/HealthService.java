530383e3a070aabfb6db65517dfc111f7414b087
George Nash <bobcaprice@hotmail.com>
2022-07-15 16:41:00 +0100

    static list of expected services now maintained in case one missing, rather than showing as down

--- gateway/src/main/java/org/orcid/memberportal/service/gateway/service/HealthService.java
+++ gateway/src/main/java/org/orcid/memberportal/service/gateway/service/HealthService.java
@@ -18,12 +18,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HealthService {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(HealthService.class);
-    
+
+    private static final String[] REQUIRED_SERVICES = new String[] { "assertionservice", "gateway", "userservice", "memberservice" };
+
     @Autowired
     private HealthClient healthClient;
-    
+
     public CompositeHealthDTO checkGlobalHealth(List<Route> routes, HttpServletRequest request) throws IOException {
         final CompositeHealthDTO globalHealth = new CompositeHealthDTO();
         globalHealth.setStatus(Status.UP);
@@ -31,9 +33,14 @@ public class HealthService {
             addHealth(globalHealth, r.getId(), r.getPrefix() + "/management/health", request);
         });
         addHealth(globalHealth, "gateway", "/management/health", request);
+        
+        if (!requiredServicesAllUp(globalHealth)) {
+            globalHealth.setStatus(Status.DOWN);
+        }
+        
         return globalHealth;
     }
-    
+
     private void addHealth(CompositeHealthDTO globalHealth, String serviceName, String endpoint, HttpServletRequest request) {
         try {
             SimpleHealthDTO health = getHealth(request, endpoint);
@@ -47,19 +54,23 @@ public class HealthService {
             globalHealth.setStatus(Status.DOWN);
         }
     }
-    
+
     private SimpleHealthDTO getHealth(HttpServletRequest request, String healthEndpoint) throws IOException {
-        URL healthCheckUrl = new URL(request.getScheme(),
-                request.getServerName(),
-                request.getServerPort(),
-                request.getContextPath().concat(healthEndpoint));
-        
+        URL healthCheckUrl = new URL(request.getScheme(), request.getServerName(), request.getServerPort(), request.getContextPath().concat(healthEndpoint));
+
         LOG.debug("Hitting health check endpoint {}", healthCheckUrl.toString());
         SimpleHealthDTO health = healthClient.getHealth(healthCheckUrl.toString());
         LOG.debug("Found health for {} - status is {}", healthCheckUrl.toString(), health.getStatus());
-        
+
         return health;
     }
-    
 
+    private boolean requiredServicesAllUp(CompositeHealthDTO globalHealth) {
+        for (String requiredService : REQUIRED_SERVICES) {
+            if (!globalHealth.getComponents().containsKey(requiredService) || !Status.UP.equals(globalHealth.getComponents().get(requiredService))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
