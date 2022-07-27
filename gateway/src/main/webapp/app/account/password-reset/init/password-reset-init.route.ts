167e5f91538f7632b4985989316a4765265c5712
Leonardo Mendoza Fernadez <leomendoza123@gmail.com>
2022-07-27 10:31:16 -0600

    Remove unrequired changes

--- gateway/src/main/webapp/app/account/password-reset/init/password-reset-init.route.ts
+++ gateway/src/main/webapp/app/account/password-reset/init/password-reset-init.route.ts
@@ -6,6 +6,7 @@ export const passwordResetInitRoute: Route = {
   path: 'reset/request',
   component: PasswordResetInitComponent,
   data: {
+    authorities: [],
     pageTitle: 'global.menu.account.password.string'
   }
 };
