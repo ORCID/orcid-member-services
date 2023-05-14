813ee54b8055b1cbf2fe98a107213c596d462a07
George Nash <bobcaprice@hotmail.com>
2023-05-14 20:28:17 +0100

    removed authorities functionality from db changes

--- user-service/src/main/java/org/orcid/memberportal/service/user/config/dbmigrations/InitialSetupMigration.java
+++ user-service/src/main/java/org/orcid/memberportal/service/user/config/dbmigrations/InitialSetupMigration.java
@@ -1,10 +1,8 @@
 package org.orcid.memberportal.service.user.config.dbmigrations;
 
-import org.orcid.memberportal.service.user.domain.User;
-import org.orcid.memberportal.service.user.security.AuthoritiesConstants;
-
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
+import org.orcid.memberportal.service.user.domain.User;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.time.Instant;
@@ -15,26 +13,9 @@ import java.time.Instant;
 @ChangeLog(order = "001")
 public class InitialSetupMigration {
 
-    @ChangeSet(order = "01", author = "initiator", id = "01-addAuthorities")
-    public void addAuthorities(MongoTemplate mongoTemplate) {
-        Authority adminAuthority = new Authority();
-        adminAuthority.setName(AuthoritiesConstants.ADMIN);
-        Authority userAuthority = new Authority();
-        userAuthority.setName(AuthoritiesConstants.USER);
-        Authority consortiumLeadAuthority = new Authority();
-        consortiumLeadAuthority.setName(AuthoritiesConstants.ASSERTION_SERVICE_ENABLED);
-        mongoTemplate.save(adminAuthority);
-        mongoTemplate.save(userAuthority);
-        mongoTemplate.save(consortiumLeadAuthority);
-    }
-
+    // note change set 2 because 1 involved adding authorities to do the db, which are no longer used
     @ChangeSet(order = "02", author = "initiator", id = "02-addUsers")
     public void addUsers(MongoTemplate mongoTemplate) {
-        Authority adminAuthority = new Authority();
-        adminAuthority.setName(AuthoritiesConstants.ADMIN);
-        Authority userAuthority = new Authority();
-        userAuthority.setName(AuthoritiesConstants.USER);
-
         User adminUser = new User();
         adminUser.setId("admin@orcid.org");
         adminUser.setPassword("$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC");
@@ -43,6 +24,7 @@ public class InitialSetupMigration {
         adminUser.setEmail("admin@orcid.org");
         adminUser.setActivated(true);
         adminUser.setLangKey("en");
+        adminUser.setAdmin(true);
         adminUser.setCreatedBy("admin@orcid.org");
         adminUser.setCreatedDate(Instant.now());
         mongoTemplate.save(adminUser);
