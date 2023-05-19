9c780f2a5c8e12f89fc3d77875d5eb74e75263b3
andrej romanov <50377758+auumgn@users.noreply.github.com>
2023-05-19 13:44:53 +0300

    add consoritum member route and components

--- gateway/src/main/webapp/app/home/home.module.ts
+++ gateway/src/main/webapp/app/home/home.module.ts
@@ -2,17 +2,28 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { GatewaySharedModule } from 'app/shared';
-import { HOME_ROUTE, HomeComponent } from './';
-import { GenericLandingComponent } from './';
-import { MemberInfoLandingComponent } from './';
-import { MemberInfoEditComponent } from './';
+import {
+  HOME_ROUTE,
+  HomeComponent,
+  ConsortiumMemberAddComponent,
+  GenericLandingComponent,
+  MemberInfoLandingComponent,
+  MemberInfoEditComponent,
+  ContactUpdateComponent
+} from './';
 import { TextFieldModule } from '@angular/cdk/text-field';
 import { QuillModule } from 'ngx-quill';
-import { ContactUpdateComponent } from './';
 
 @NgModule({
   imports: [GatewaySharedModule, RouterModule.forChild([HOME_ROUTE]), TextFieldModule, QuillModule.forRoot()],
-  declarations: [HomeComponent, GenericLandingComponent, MemberInfoLandingComponent, MemberInfoEditComponent, ContactUpdateComponent],
+  declarations: [
+    HomeComponent,
+    GenericLandingComponent,
+    MemberInfoLandingComponent,
+    MemberInfoEditComponent,
+    ContactUpdateComponent,
+    ConsortiumMemberAddComponent
+  ],
 
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
