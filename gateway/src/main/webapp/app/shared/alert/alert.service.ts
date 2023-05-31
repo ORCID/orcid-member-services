b081662afdff163d434d3bff3eae0734eb4a314b
andrej romanov <50377758+auumgn@users.noreply.github.com>
2023-05-31 19:15:46 +0300

    improve alert service

--- gateway/src/main/webapp/app/shared/alert/alert.service.ts
+++ gateway/src/main/webapp/app/shared/alert/alert.service.ts
@@ -1,30 +1,47 @@
 import { ComponentType } from '@angular/cdk/portal';
-import { ComponentFactoryResolver, ComponentRef, Injectable, ViewContainerRef } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { ComponentFactoryResolver, ComponentRef, Injectable, Injector, ReflectiveInjector, ViewContainerRef } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+interface AlertInterface {
+  alertComponent: ComponentType<any>;
+  data?: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertService {
-  activeAlert: BehaviorSubject<ComponentType<any>> = new BehaviorSubject(null);
   alertRef: ComponentRef<any>;
+  containerRefSubj = new ReplaySubject<ViewContainerRef>(1);
 
   constructor(private resolver: ComponentFactoryResolver) {}
 
-  showHomepageLightboxModal(containerRef: ViewContainerRef): void {
-    console.log(this.activeAlert.value);
+  setupContainerRef(containerRef: ViewContainerRef) {
+    this.containerRefSubj.next(containerRef);
+  }
 
-    if (this.activeAlert.value) {
-      const popupFactory = this.resolver.resolveComponentFactory(this.activeAlert.value);
-      this.alertRef = containerRef.createComponent(popupFactory);
-      this.alertRef.instance.hide = () => {
-        this.hideHomepageLightboxModal();
-      };
-    }
+  showHomepageLightboxModal(alert: AlertInterface): void {
+    this.containerRefSubj.pipe(take(1)).subscribe(containerRef => {
+      if (alert.alertComponent) {
+        const popupFactory = this.resolver.resolveComponentFactory(alert.alertComponent);
+        const injector: Injector = ReflectiveInjector.resolveAndCreate([
+          {
+            provide: 'config',
+            useValue: {
+              data: alert.data
+            }
+          }
+        ]);
+        this.alertRef = containerRef.createComponent(popupFactory, 0, injector);
+        this.alertRef.instance.hide = () => {
+          this.hideHomepageLightboxModal();
+        };
+      }
+    });
   }
 
   hideHomepageLightboxModal(): void {
     this.alertRef.destroy();
-    this.activeAlert.next(null);
   }
 }
