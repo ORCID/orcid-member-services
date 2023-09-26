56b37dc5149fb9e851c24e7591faa515d1988d38
Jason Thomas <j.thomas@orcid.org>
2023-09-26 10:25:52 +0100

    search and replace gateway/ui

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -22,16 +22,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gateway'`, () => {
+  it(`should have as title 'ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gateway');
+    expect(app.title).toEqual('ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gateway app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ui app is running!');
   });
 });
