3e49683b54e770de1dea27c77732be18571feafd
andrej romanov <50377758+auumgn@users.noreply.github.com>
2023-10-04 16:03:43 +0300

    separate routes

--- ui/src/app/account/account.module.ts
+++ ui/src/app/account/account.module.ts
@@ -3,14 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
-const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  }
-];
-
+import { routes } from './account.route';
 
 @NgModule({
   declarations: [
---
+++ ui/src/app/account/account.route.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+
+export const routes: Routes = [
+    {
+      path: 'login',
+      component: LoginComponent
+    }
+  ];
+  
