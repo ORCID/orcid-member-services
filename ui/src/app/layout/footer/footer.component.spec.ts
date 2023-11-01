1229706857dee62dfd8d577811dd7795349e0f80
George Nash <bobcaprice@hotmail.com>
2023-11-01 13:46:24 +0000

    footer test

--- ui/src/app/layout/footer/footer.component.spec.ts
+++ ui/src/app/layout/footer/footer.component.spec.ts
@@ -1,21 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { FooterComponent } from './footer.component';
+import { FooterComponent } from './footer.component'
+import { By } from '@angular/platform-browser'
+import { AccountService } from 'src/app/account/service/account.service'
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+  let component: FooterComponent
+  let fixture: ComponentFixture<FooterComponent>
+  let accountService: jasmine.SpyObj<AccountService>
 
   beforeEach(() => {
+    const accountServiceSpy = jasmine.createSpyObj('AccountService', ['isAuthenticated'])
+
     TestBed.configureTestingModule({
-      declarations: [FooterComponent]
-    });
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      declarations: [FooterComponent],
+      providers: [{ provide: AccountService, useValue: accountServiceSpy }],
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(FooterComponent)
+    accountService = TestBed.inject(AccountService) as jasmine.SpyObj<AccountService>
+    fixture.detectChanges()
+    component = fixture.componentInstance
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should contain copyright notice', () => {
+    const copyright = fixture.debugElement.query(By.css('.copyright'))
+    expect(copyright).toBeTruthy()
+  })
+
+  it('if not authenticated, should not contain help link', () => {
+    accountService.isAuthenticated.and.returnValue(false)
+    fixture.detectChanges()
+    const helpLink = fixture.debugElement.query(By.css('#helpLink'))
+    expect(helpLink).toBeFalsy()
+  })
+
+  it('if authenticated, should contain help link', () => {
+    accountService.isAuthenticated.and.returnValue(true)
+    fixture.detectChanges()
+    const helpLink = fixture.debugElement.query(By.css('#helpLink'))
+    expect(helpLink).toBeTruthy()
+  })
+})
