d3704525eae94f189b74efc74f2abda3a63c4e53
George Nash <bobcaprice@hotmail.com>
2024-01-24 09:12:51 +0000

    fixed lint

--- ui/src/app/user/users.component.ts
+++ ui/src/app/user/users.component.ts
@@ -36,7 +36,7 @@ export class UsersComponent implements OnInit, OnDestroy {
   totalItems: any
   itemsPerPage: any
   page: any
-  sortColumn: string = 'id'
+  sortColumn = 'id'
   ascending: any
   itemCount: string | null | undefined = null
   searchTerm: string | null = null
