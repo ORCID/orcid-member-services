b7ddf81bf67b522e04dd45d0fbd93e89f5fbaf38
George Nash <bobcaprice@hotmail.com>
2024-03-02 00:40:58 +0000

    removed unused import

--- ui/src/app/error/error-alert.component.spec.ts
+++ ui/src/app/error/error-alert.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { ErrorAlertComponent } from './error-alert.component'
-import { ErrorService } from './service/error.service'
 import { AppModule } from 'src/app/app.module'
 
 describe('ErrorAlertComponent', () => {
