b5ab36970382b9adf5ea739babbbae184018ef9b
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-03-07 13:53:27 +0200

    fix affiliation status i18n

--- ui/src/app/shared/pipe/localize.ts
+++ ui/src/app/shared/pipe/localize.ts
@@ -17,6 +17,37 @@ export class LocalizePipe implements PipeTransform {
         return $localize`:@@userServiceApp.user.updated.string:User updated successfully`
       case AlertType.USER_DELETED:
         return $localize`:@@userServiceApp.user.deleted.string:User deleted successfully`
+
+      // Affiliation pretty statuses
+
+      case 'User denied access':
+        return $localize`:@@gatewayApp.assertionStatus.userDeniedAccess.string:User denied access`
+      case 'Pending':
+        return $localize`:@@gatewayApp.assertionStatus.pending.string:Pending`
+      case 'In ORCID':
+        return $localize`:@@gatewayApp.assertionStatus.inOrcid.string:In ORCID`
+      case 'User granted access':
+        return $localize`:@@gatewayApp.assertionStatus.userGrantedAccess.string:User granted access`
+      case 'User deleted from ORCID':
+        return $localize`:@@gatewayApp.assertionStatus.userDeletedFromOrcid.string:User deleted from ORCID`
+      case 'User revoked access':
+        return $localize`:@@gatewayApp.assertionStatus.userRevokedAccess.string:User revoked access`
+      case 'Error adding to ORCID':
+        return $localize`:@@gatewayApp.assertionStatus.errorAddingToOrcid.string:Error adding to ORCID`
+      case 'Error updating in ORCID':
+        return $localize`:@@gatewayApp.assertionStatus.errorUpdatingInOrcid.string:Error updating in ORCID`
+      case 'Pending retry in ORCID':
+        return $localize`:@@gatewayApp.assertionStatus.pendingRetryInOrcid.string:Pending retry in ORCID`
+      case 'Error deleting in ORCID':
+        return $localize`:@@gatewayApp.assertionStatus.errorDeletingInOrcid.string:Error deleting in ORCID`
+      case 'Notification requested':
+        return $localize`:@@gatewayApp.assertionStatus.notificationRequested.string:Notification requested`
+      case 'Notification sent':
+        return $localize`:@@gatewayApp.assertionStatus.notificationSent.string:Notification sent`
+      case 'Notification failed':
+        return $localize`:@@gatewayApp.assertionStatus.notificationFailed.string:Notification failed`
+      case 'Pending update in ORCID':
+        return $localize`:@@gatewayApp.assertionStatus.pendingUpdateInOrcid.string:Pending update in ORCID`
     }
   }
 }
