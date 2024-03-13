9c0ac0c1e4dee566e93c4f913db0e616166625c6
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-03-13 14:00:26 +0200

    add date util service

---
+++ ui/src/app/shared/service/date-util.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core'
+import * as moment from 'moment'
+import { DEFAULT_EARLIEST_YEAR } from '../constants/orcid-api.constants'
+
+@Injectable({ providedIn: 'root' })
+export class DateUtilService {
+  getCurrentMonthNumber(): number {
+    return +moment().format('M')
+  }
+
+  getCurrentYear(): number {
+    return moment().year()
+  }
+
+  getYearsList(futureYears?: number) {
+    if (!futureYears) {
+      futureYears = 0
+    }
+    const years = []
+    const dateStart = moment(DEFAULT_EARLIEST_YEAR, 'YYYY')
+    const dateEnd = moment().add(futureYears - 1, 'y')
+
+    while (dateEnd.diff(dateStart, 'years') >= 0) {
+      years.push(dateStart.format('YYYY'))
+      dateStart.add(1, 'year')
+    }
+    return years.reverse()
+  }
+
+  getFutureYearsIncludingCurrent(futureYears: number) {
+    const years = []
+    const yearStart: number = moment().year()
+
+    for (let i = 0; i < futureYears + 1; i++) {
+      years.push(yearStart + i)
+    }
+    return years
+  }
+
+  getMonthsList(): [number, string][] {
+    const months = moment.months()
+    const monthsArray = []
+    for (let _i = 1; _i <= months.length; _i++) {
+      let res: [number, string] = [parseInt(('0' + _i.toString()).slice(-2)), months[_i - 1]]
+      monthsArray.push(res)
+    }
+    return monthsArray
+  }
+
+  getFutureMonthsList(): [number, string][] {
+    const months = moment.months()
+    const monthsArray = []
+    for (let _i = 1; _i <= months.length; _i++) {
+      if (_i > this.getCurrentMonthNumber()) {
+        let res: [number, string] = [parseInt(('0' + _i.toString()).slice(-2)), months[_i - 1]]
+        monthsArray.push(res)
+      }
+    }
+    return monthsArray
+  }
+
+  getDaysList(year?: string, month?: string) {
+    let daysInMonth
+    if (year && month) {
+      daysInMonth = moment(year + '-' + month, 'YYYY-MM').daysInMonth()
+    } else {
+      daysInMonth = moment().daysInMonth()
+    }
+    const days = []
+    for (let _i = 1; _i <= daysInMonth; _i++) {
+      days.push(('0' + _i.toString()).slice(-2))
+    }
+    return days
+  }
+}
