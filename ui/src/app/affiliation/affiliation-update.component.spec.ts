dd057159987c2c6df02ef88d7497d9577e9de8b7
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-03-13 17:49:19 +0200

    fix test

--- ui/src/app/affiliation/affiliation-update.component.spec.ts
+++ ui/src/app/affiliation/affiliation-update.component.spec.ts
@@ -1,21 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { AffiliationUpdateComponent } from './affiliation-update.component';
+import { AffiliationUpdateComponent } from './affiliation-update.component'
+import { HttpClientModule } from '@angular/common/http'
 
 describe('AffiliationUpdateComponent', () => {
-  let component: AffiliationUpdateComponent;
-  let fixture: ComponentFixture<AffiliationUpdateComponent>;
+  let component: AffiliationUpdateComponent
+  let fixture: ComponentFixture<AffiliationUpdateComponent>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AffiliationUpdateComponent]
-    });
-    fixture = TestBed.createComponent(AffiliationUpdateComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      declarations: [AffiliationUpdateComponent],
+      imports: [HttpClientModule],
+    })
+    fixture = TestBed.createComponent(AffiliationUpdateComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
