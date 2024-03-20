c6ff1e78bb99623ebe54e720ecb6e8b37dbaff68
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-03-20 16:58:25 +0200

    add i18n

--- ui/src/app/affiliation/affiliation-delete.component.spec.ts
+++ ui/src/app/affiliation/affiliation-delete.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { AffiliationDeletePopupComponent } from './affiliation-delete.component'
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('AffiliationDeleteComponent', () => {
   let component: AffiliationDeletePopupComponent
@@ -9,6 +10,7 @@ describe('AffiliationDeleteComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AffiliationDeletePopupComponent],
+      imports: [RouterTestingModule],
     })
     fixture = TestBed.createComponent(AffiliationDeletePopupComponent)
     component = fixture.componentInstance
