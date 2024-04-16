19754561131c5df5fb42ae78d3ea28c30e4513a9
George Nash <bobcaprice@hotmail.com>
2024-04-16 11:30:57 +0100

    added generic-landing component

--- ui/src/app/home/home.module.ts
+++ ui/src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { routes } from './home.route'
-import { HomeComponent } from './home.component'
+import { HomeComponent } from './home.component';
+import { GenericLandingComponent } from './generic-landing.component'
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, GenericLandingComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class HomeModule {}
