09d0f7628a5c5fb5e9a684bebf9631eb4918fefd
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-04-19 20:00:41 +0300

    fix home component tests

--- ui/src/app/home/home.component.spec.ts
+++ ui/src/app/home/home.component.spec.ts
@@ -1,32 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { HomeComponent } from './home.component'
-import { MemberService } from '../member/service/member.service'
 import { AccountService } from '../account'
 import { of } from 'rxjs'
 
 describe('HomeComponent', () => {
   let component: HomeComponent
   let fixture: ComponentFixture<HomeComponent>
-  let memberServiceSpy: jasmine.SpyObj<MemberService>
   let accountServiceSpy: jasmine.SpyObj<AccountService>
 
   beforeEach(() => {
     accountServiceSpy = jasmine.createSpyObj('AccountService', ['getAccountData'])
-    memberServiceSpy = jasmine.createSpyObj('MemberService', ['getMemberData'])
 
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
-      providers: [
-        { provide: MemberService, useValue: memberServiceSpy },
-        { provide: AccountService, useValue: accountServiceSpy },
-      ],
+      providers: [{ provide: AccountService, useValue: accountServiceSpy }],
     })
     fixture = TestBed.createComponent(HomeComponent)
     component = fixture.componentInstance
 
     accountServiceSpy = TestBed.inject(AccountService) as jasmine.SpyObj<AccountService>
-    memberServiceSpy = TestBed.inject(MemberService) as jasmine.SpyObj<MemberService>
   })
 
   it('should call getAccountData but not getMemberData', () => {
@@ -37,7 +30,7 @@ describe('HomeComponent', () => {
     component.ngOnInit()
 
     expect(accountServiceSpy.getAccountData).toHaveBeenCalled()
-    expect(memberServiceSpy.getMemberData).toHaveBeenCalledTimes(0)
+    expect(component.loggedInMessage).toBeUndefined()
   })
 
   it('should call getMemberData if account data is not null', () => {
@@ -57,13 +50,12 @@ describe('HomeComponent', () => {
         mfaEnabled: false,
       })
     )
-    memberServiceSpy.getMemberData.and.returnValue(of({}))
 
     expect(component).toBeTruthy()
 
     component.ngOnInit()
 
     expect(accountServiceSpy.getAccountData).toHaveBeenCalled()
-    expect(memberServiceSpy.getMemberData).toHaveBeenCalled()
+    expect(component.loggedInMessage).toEqual('You are logged in as user email@email.com')
   })
 })
