4630cadd8b5cbe6006adf60f054b16682e9bae53
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-05-13 15:13:14 +0300

    tweak pagination test

--- ui/cypress/e2e/retention/ui-pagination.cy.js
+++ ui/cypress/e2e/retention/ui-pagination.cy.js
@@ -1,51 +1,58 @@
 /// <reference types="cypress" />
-import data from '../../fixtures/test-data.json';
-import credentials from '../../fixtures/credentials.json';
+import data from "../../fixtures/test-data.json";
+import credentials from "../../fixtures/credentials.json";
 
-describe('Test pagination', () => {
+describe("Test pagination", () => {
   beforeEach(() => {
-    cy.programmaticSignin(data.paginationMember.users.owner.email, credentials.password);
-  })
+    cy.programmaticSignin(
+      data.paginationMember.users.owner.email,
+      credentials.password,
+    );
+  });
 
   afterEach(() => {
     cy.programmaticSignout();
-  })
-
-  it('Test the "Manage users" page', function() {
-    cy.visit('/user');
-    /*
-    cy.get('.container-fluid').within(() => {
-      cy.get('p')
-      .contains('Showing 1 - 20 of 22 items')
-    })*/
-    cy.get('tbody').children().should('have.length', 20);
-    cy.get('.pagination').contains("1");
-    cy.get('.pagination').contains("3").should('not.exist');
-    cy.get('.pagination').contains("2").click();
-    cy.get('tbody').children().should('have.length', 2);
-    cy.get('tbody').within(() => {
-      cy.get('td').contains(data.paginationMember.assertionEmail1).should('not.exist');
-      cy.get('td').contains(data.paginationMember.assertionEmail2);
-      cy.get('td').contains(data.paginationMember.assertionEmail3);
-    })
   });
 
-  it('Test the "Affiliations" page', function() {
-    cy.visit('/assertion');
+  it('Test the "Manage users" page', function () {
+    cy.visit("ui/en/users");
     /*
     cy.get('.container-fluid').within(() => {
       cy.get('p')
       .contains('Showing 1 - 20 of 22 items')
     })*/
-    cy.get('tbody').children().should('have.length', 20);
-    cy.get('.pagination').contains("1");
-    cy.get('.pagination').contains("2");
-    cy.get('.pagination').contains("4").should('not.exist');
-    cy.get('.pagination').contains("3").click();
-    cy.get('tbody').children().should('have.length', 2);
-    cy.get('tbody').within(() => {
-      cy.get('td').contains(data.paginationMember.assertionData1).should('not.exist');
-      cy.get('td').contains(data.paginationMember.assertionData2);
-    })
+    cy.get("tbody").children().should("have.length", 20);
+    cy.get(".pagination").contains("1");
+    cy.get(".pagination").contains("3").should("not.exist");
+    cy.get(".pagination").contains("2").click();
+    cy.get("tbody").children().should("have.length", 2);
+    cy.get("tbody").within(() => {
+      cy.get("td")
+        .contains(data.paginationMember.assertionEmail1)
+        .should("not.exist");
+      cy.get("td").contains(data.paginationMember.assertionEmail2);
+      cy.get("td").contains(data.paginationMember.assertionEmail3);
+    });
+  });
+
+  it('Test the "Affiliations" page', function () {
+    cy.visit("ui/en/affiliations");
+    /*
+    cy.get('.container-fluid').within(() => {
+      cy.get('p')
+      .contains('Showing 1 - 20 of 22 items')
+    })*/
+    cy.get("tbody").children().should("have.length", 20);
+    cy.get(".pagination").contains("1");
+    cy.get(".pagination").contains("2");
+    cy.get(".pagination").contains("4").should("not.exist");
+    cy.get(".pagination").contains("3").click();
+    cy.get("tbody").children().should("have.length", 2);
+    cy.get("tbody").within(() => {
+      cy.get("td")
+        .contains(data.paginationMember.assertionData1)
+        .should("not.exist");
+      cy.get("td").contains(data.paginationMember.assertionData2);
+    });
   });
 });
