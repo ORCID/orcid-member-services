32ea7fdc0339509136733e5ddcb9cc3e6de9de69
andrej romanov <50377758+auumgn@users.noreply.github.com>
2024-06-12 14:12:03 +0300

    fix test

--- ui/src/app/home/home.component.spec.ts
+++ ui/src/app/home/home.component.spec.ts
@@ -56,6 +56,6 @@ describe('HomeComponent', () => {
     component.ngOnInit()
 
     expect(accountServiceSpy.getAccountData).toHaveBeenCalled()
-    expect(component.loggedInMessage).toEqual('You are logged in as user email@email.com')
+    expect(component.loggedInMessage).toEqual('You are logged in as user <strong>email@email.com</strong>')
   })
 })
