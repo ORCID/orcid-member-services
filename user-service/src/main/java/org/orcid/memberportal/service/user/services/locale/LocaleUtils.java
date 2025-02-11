49248ab321615c486cd622c7908e12e4fc2608c9
George Nash <gnash@george-work-laptop.local>
2025-02-11 17:26:59 +0000

    locale utils will now translate chinese language codes with hyphens to locales

--- user-service/src/main/java/org/orcid/memberportal/service/user/services/locale/LocaleUtils.java
+++ user-service/src/main/java/org/orcid/memberportal/service/user/services/locale/LocaleUtils.java
@@ -5,18 +5,18 @@ import java.util.Locale;
 import java.util.Map;
 
 public class LocaleUtils {
-    
+
     private static final Map<String, Locale> CUSTOM_LOCALES;
-    
+
     static {
         CUSTOM_LOCALES = new HashMap<>();
-        CUSTOM_LOCALES.put("zh_TW", Locale.TRADITIONAL_CHINESE);
-        CUSTOM_LOCALES.put("zh_CN", Locale.SIMPLIFIED_CHINESE);
+        CUSTOM_LOCALES.put("zh-TW", Locale.TRADITIONAL_CHINESE);
+        CUSTOM_LOCALES.put("zh-CN", Locale.SIMPLIFIED_CHINESE);
     }
-    
+
     /**
      * Returns Locale object for member portal custom locale if specified, otherwise creates a new Locale.
-     * 
+     *
      * @param langKey
      * @return
      */
