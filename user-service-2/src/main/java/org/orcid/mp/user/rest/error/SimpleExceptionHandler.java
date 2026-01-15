d750ac0a0295f0ceece93d32ca823d33b0fd467a
George Nash <gnash@george-work-laptop.local>
2026-01-15 23:16:16 +0000

    fixed security bug with method security not activated

--- user-service-2/src/main/java/org/orcid/mp/user/rest/error/SimpleExceptionHandler.java
+++ user-service-2/src/main/java/org/orcid/mp/user/rest/error/SimpleExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -59,6 +60,11 @@ public class SimpleExceptionHandler {
     @ExceptionHandler(UserNotLoggedInException.class)
     public ResponseEntity<String> handleUserNotLoggedInException(UserNotLoggedInException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in");
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<String> handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied");
     }
 
     @ExceptionHandler(Exception.class)
