e15911bc91296917be3108d68596efca5d7612c0
Giles Westwood <gwestwood@gwestwood-m2-pro.local>
2026-01-29 10:11:56 +0000

    add timeouts and retrys to the http connector

--- assertion-service/src/main/java/org/orcid/memberportal/service/assertion/config/MailConfiguration.java
+++ assertion-service/src/main/java/org/orcid/memberportal/service/assertion/config/MailConfiguration.java
@@ -4,8 +4,10 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.orcid.memberportal.service.assertion.mail.client.impl.MailgunClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +22,21 @@ public class MailConfiguration {
         CredentialsProvider provider = new BasicCredentialsProvider();
         provider.setCredentials(AuthScope.ANY, credentials);
 
-        HttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+        // Configure timeouts to handle NAT gateway latency
+        RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectTimeout(30000)           // 30 seconds to establish connection
+            .setConnectionRequestTimeout(30000) // 30 seconds to get connection from pool
+            .setSocketTimeout(60000)            // 60 seconds to wait for data
+            .build();
+
+        // Configure retry handler for transient network issues
+        StandardHttpRequestRetryHandler retryHandler = new StandardHttpRequestRetryHandler(3, true);
+
+        HttpClient client = HttpClientBuilder.create()
+            .setDefaultCredentialsProvider(provider)
+            .setDefaultRequestConfig(requestConfig)
+            .setRetryHandler(retryHandler)
+            .build();
         return client;
     }
     
