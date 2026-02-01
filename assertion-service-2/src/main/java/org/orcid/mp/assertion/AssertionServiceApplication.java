fbe7126335c615c773b7364002a036c311ef4cbb
George Nash <gnash@george-work-laptop.local>
2026-02-01 18:47:32 +0000

    new assertion service shell

---
+++ assertion-service-2/src/main/java/org/orcid/mp/assertion/AssertionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.orcid.mp.assertion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssertionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssertionServiceApplication.class, args);
+	}
+
+}
---
+++ assertion-service-2/src/test/java/org/orcid/mp/assertion/AssertionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.orcid.mp.assertion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssertionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
