3a4d3edd607df65f96692e0016a76aa36ee23b23
George Nash <gnash@george-work-laptop.local>
2026-03-30 12:47:26 +0100

    another url correction

--- ui-2/src/app/landing-page/landing-page.service.ts
+++ ui-2/src/app/landing-page/landing-page.service.ts
@@ -9,7 +9,7 @@ export class LandingPageService {
   private headers: HttpHeaders
 
   idTokenUri = '/assertionservice/id-token'
-  recordConnectionUri = '/assertionservice/api/assertion/record/'
+  recordConnectionUri = '/assertionservice/assertion/record/'
   memberInfoUri = '/memberservice/api/members/authorized/'
   userInfoUri = ORCID_BASE_URL + '/oauth/userinfo'
   publicKeyUri = ORCID_BASE_URL + '/oauth/jwks'
