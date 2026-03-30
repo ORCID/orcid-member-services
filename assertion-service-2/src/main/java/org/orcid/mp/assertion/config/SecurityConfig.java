695b493e0a0157fb27524c6abe96a63e4e501a57
George Nash <gnash@george-work-laptop.local>
2026-03-30 15:30:44 +0100

    updated path to record in security config

--- assertion-service-2/src/main/java/org/orcid/mp/assertion/config/SecurityConfig.java
+++ assertion-service-2/src/main/java/org/orcid/mp/assertion/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/id-token", "/assertion/record/**").permitAll()
+                        .requestMatchers("/id-token", "/record/**").permitAll()
                         .requestMatchers("/internal/**").hasAuthority("SCOPE_internal")
                         .anyRequest().authenticated()
                 )
