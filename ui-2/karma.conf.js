facfb5eea97891faf293f0a8a71679aed16cd813
Daniel Palafox <daniel.p4l4fox@gmail.com>
2026-04-13 01:03:03 -0400

    chore: update Github CI node version and update ChromeHeadlessCI config

--- ui-2/karma.conf.js
+++ ui-2/karma.conf.js
@@ -33,7 +33,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["ChromeHeadless"],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-gpu"],
+      },
+    },
+    browsers: [process.env.CI ? "ChromeHeadlessCI" : "ChromeHeadless"],
     singleRun: true,
     restartOnFileChange: true,
   });
