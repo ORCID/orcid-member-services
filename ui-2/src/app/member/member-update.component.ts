4b0f425a4e62dd308237aada9f9959d24807a563
Daniel Palafox <daniel.p4l4fox@gmail.com>
2026-06-04 12:19:59 -0400

    fix: rebase issues

--- ui-2/src/app/member/member-update.component.ts
+++ ui-2/src/app/member/member-update.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
 import { FormBuilder, FormControl, Validators } from '@angular/forms'
 import { ActivatedRoute, Router } from '@angular/router'
 import { Observable } from 'rxjs'
