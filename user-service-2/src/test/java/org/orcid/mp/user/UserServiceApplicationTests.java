ffc138fb300f6dedfc9b2b8d98010ec87747d76b
Daniel Palafox <daniel.p4l4fox@gmail.com>
2026-06-12 12:23:46 -0400

    test: fix mongo test issue

--- user-service-2/src/test/java/org/orcid/mp/user/UserServiceApplicationTests.java
+++ user-service-2/src/test/java/org/orcid/mp/user/UserServiceApplicationTests.java
@@ -2,19 +2,14 @@ package org.orcid.mp.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import com.mongodb.client.MongoClient;
+import org.togglz.core.repository.StateRepository;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
 	@MockitoBean
-    private MongoTemplate mongoTemplate;
-
-    @MockitoBean
-    private MongoClient mongoClient;
+	private StateRepository stateRepository;
 
 	@Test
 	void contextLoads() {
