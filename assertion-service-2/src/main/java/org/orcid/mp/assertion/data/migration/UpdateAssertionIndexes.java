64130375e0aa32fdd5dd8ecc58df59587ae8fc86
George Nash <gnash@george-work-laptop.local>
2026-09-08 18:00:48 +0100

    removed legacy index and added new compound one to address performance issues

---
+++ assertion-service-2/src/main/java/org/orcid/mp/assertion/data/migration/UpdateAssertionIndexes.java
@@ -0,0 +1,53 @@
+package org.orcid.mp.assertion.data.migration;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.orcid.mp.assertion.domain.Assertion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+
+@ChangeUnit(id = "update-assertion-indexes", order = "010", author = "George Nash")
+public class UpdateAssertionIndexes {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UpdateAssertionIndexes.class);
+    private static final String OLD_INDEX_NAME = "added_to_orcid_1_created_1_status_1";
+    private static final String NEW_INDEX_NAME = "token_status_updated_added_created_idx";
+
+    @Execution
+    public void execution(MongoTemplate mongoTemplate) {
+        IndexOperations indexOps = mongoTemplate.indexOps(Assertion.class);
+
+        try {
+            indexOps.dropIndex(OLD_INDEX_NAME);
+            LOG.info("Successfully dropped legacy index: {}", OLD_INDEX_NAME);
+        } catch (Exception e) {
+            LOG.warn("Legacy index {} was not found or could not be dropped: {}", OLD_INDEX_NAME, e.getMessage());
+        }
+
+        Index newIndex = new Index()
+                .on("token_available", Sort.Direction.ASC)
+                .on("status", Sort.Direction.ASC)
+                .on("updated_in_orcid", Sort.Direction.ASC)
+                .on("added_to_orcid", Sort.Direction.ASC)
+                .on("created", Sort.Direction.ASC)
+                .named(NEW_INDEX_NAME);
+
+        indexOps.createIndex(newIndex);
+        LOG.info("Successfully created optimized index: {}", NEW_INDEX_NAME);
+    }
+
+    @RollbackExecution
+    public void rollbackExecution(MongoTemplate mongoTemplate) {
+        IndexOperations indexOps = mongoTemplate.indexOps(Assertion.class);
+        try {
+            indexOps.dropIndex(NEW_INDEX_NAME);
+        } catch (Exception e) {
+            LOG.warn("Could not drop index during rollback: {}", e.getMessage());
+        }
+    }
+}
